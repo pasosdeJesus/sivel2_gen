c45c09eb73d47be1ec4fcc1732af502716cc6941
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-09-03 07:19:35 -0500

    datos

--- db/datos-basicas.sql
+++ db/datos-basicas.sql
@@ -16083,6 +16083,31 @@ SET client_min_messages = warning;
 SET search_path = public, pg_catalog;
 
 --
+-- Data for Name: regionsjr; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO regionsjr (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (1, 'SIN INFORMACIÓN', '2014-09-01', NULL, '2014-09-01 12:54:42.630253', '2014-09-03 10:40:11.967004');
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+--
 -- Data for Name: tsitio; Type: TABLE DATA; Schema: public; Owner: -
 --
 
