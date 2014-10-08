da36839d4cd3d0f05aa2b44b4be741a2558e5739
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-10-07 22:03:08 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Hace prueba de regresión y envia a github
 
+bundle update
+bundle install
 grep "^ *gem *.debugger*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
 	echo "Gemfile incluye debugger que heroku no quiere"
