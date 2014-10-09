a4faa5e51218202d241f146fadd1796b430e0748
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-10-09 11:14:13 -0500

    Basicas con derechos

--- app/controllers/hogar_controller.rb
+++ app/controllers/hogar_controller.rb
@@ -5,9 +5,13 @@ class HogarController < ApplicationController
 		authorize! :manage, :tablasbasicas
     @ntablas = {}
     Ability::tablasbasicas.each { |t|
+      #puts "t=#{t}"
       k = t.capitalize.constantize 
+      #puts "k=#{k}"
       n = k.human_attribute_name(t.pluralize.capitalize) 
+      #puts "n=#{n}"
       @ntablas[n] = send("admin_" + t.pluralize + "_path")
+      #puts "@ntablas[n]=#{@ntablas[n]}"
     } 
     @ntablasor = @ntablas.keys.localize(:es).sort.to_a
 	end
