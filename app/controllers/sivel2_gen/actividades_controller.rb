c573ccc2e0468f48f60202b0fcc3649634c26a69
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-11-18 20:32:58 -0500

    actividades opera

--- app/controllers/sivel2_gen/actividades_controller.rb
+++ app/controllers/sivel2_gen/actividades_controller.rb
@@ -1,13 +1,16 @@
 # encoding: UTF-8
+require_dependency "sivel2_gen/application_controller"
+
 module Sivel2Gen
   class ActividadesController < ApplicationController
     before_action :set_actividad, only: [:show, :edit, :update, :destroy]
-    load_and_authorize_resource
+    load_and_authorize_resource class: Sivel2Gen::Actividad
 
     # GET /actividades
     # GET /actividades.json
     def index
-      @actividades = @actividades.order(fecha: :desc).paginate(:page => params[:pagina], per_page: 20)
+      @actividades = Actividad.order(fecha: :desc).paginate(
+        :page => params[:pagina], per_page: 20)
     end
 
     # GET /actividades/1
@@ -17,6 +20,7 @@ module Sivel2Gen
 
     # GET /actividades/new
     def new
+      @actividad = Actividad.new
       @actividad.current_usuario = current_usuario
       @actividad.regionsjr_id = current_usuario.regionsjr_id.nil? ?  
         1 : current_usuario.regionsjr_id
@@ -29,16 +33,19 @@ module Sivel2Gen
     # POST /actividades
     # POST /actividades.json
     def create
+      @actividad = Actividad.new(actividad_params)
       @actividad.current_usuario = current_usuario
-
       respond_to do |format|
         if @actividad.save
           format.html { redirect_to @actividad, notice: 'Actividad creada.' }
-          format.json { render action: 'show', status: :created, location: @actividad }
+          format.json { 
+            render action: 'show', status: :created, location: @actividad 
+          }
         else
-          #params[:actividad].delete(:anexoactividad_attributes)
           format.html { render action: 'new' }
-          format.json { render json: @actividad.errors, status: :unprocessable_entity }
+          format.json { 
+            render json: @actividad.errors, status: :unprocessable_entity 
+          }
         end
       end
     end
@@ -48,11 +55,15 @@ module Sivel2Gen
     def update
       respond_to do |format|
         if @actividad.update(actividad_params)
-          format.html { redirect_to @actividad, notice: 'Actividad actualizada.' }
+          format.html { 
+            redirect_to @actividad, notice: 'Actividad actualizada.' 
+          }
           format.json { head :no_content }
         else
           format.html { render action: 'edit' }
-          format.json { render json: @actividad.errors, status: :unprocessable_entity }
+          format.json { 
+            render json: @actividad.errors, status: :unprocessable_entity 
+          }
         end
       end
     end
@@ -62,12 +73,14 @@ module Sivel2Gen
     def destroy
       @actividad.destroy
       respond_to do |format|
-        format.html { redirect_to actividades_url }
+        format.html { 
+        redirect_to actividades_url, notice: 'Actividad eliminada' }
         format.json { head :no_content }
       end
     end
 
     private
+
     def set_actividad
       @actividad = Actividad.find(params[:id])
       @actividad.current_usuario = current_usuario
@@ -75,13 +88,18 @@ module Sivel2Gen
 
     # No confiar parametros a Internet, sólo permitir lista blanca
     def actividad_params
-      params.require(:actividad).permit(:regionsjr_id, :minutos, :nombre, 
-                                        :objetivo, :proyecto, :resultado, :fecha, :actividad, 
-                                        :observaciones, :actividadarea_ids => [],
-                                        :actividad_rangoedadac_attributes => 
-      [:id, :rangoedadac_id, :fl, :fr, :ml, :mr, :_destroy] ,
-        :anexoactividad_attributes => [:id, :descripcion, :adjunto, :_destroy]
-                                       )
+      params.require(:actividad).permit(
+        :regionsjr_id, :minutos, :nombre, 
+        :objetivo, :proyecto, :resultado,
+        :fecha, :actividad, :observaciones, 
+        :actividadarea_ids => [],
+        :actividad_rangoedadac_attributes => [
+          :id, :rangoedadac_id, :fl, :fr, :ml, :mr, :_destroy
+        ],
+        :anexoactividad_attributes => [
+          :id, :descripcion, :adjunto, :_destroy
+        ]
+      )
     end
   end
 end
