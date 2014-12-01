49326ee4e8df8d005fc961d296d21d2f3d6ba704
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-12-01 16:23:39 -0500

    controladores

---
+++ app/controllers/sivel2_gen/admin/estadosciviles_controller.rb
@@ -0,0 +1,28 @@
+# encoding: UTF-8
+module Sivel2Gen
+  module Admin
+    class EstadoscivilesController < Sivel2Gen::Admin::BasicasController
+      before_action :set_estadocivil, only: [:show, :edit, :update, :destroy]
+      load_and_authorize_resource class: Sivel2Gen::Estadocivil
+
+      def clase 
+        "Sivel2Gen::Estadocivil"
+      end
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_estadocivil
+        @basica = Sivel2Gen::Estadocivil.find(params[:id])
+      end
+
+      def genclase
+        return 'M';
+      end
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def estadocivil_params
+        params.require(:estadocivil).permit(*atributos_form)
+      end
+
+    end
+  end
+end
---
+++ app/controllers/sivel2_gen/admin/maternidades_controller.rb
@@ -0,0 +1,28 @@
+# encoding: UTF-8
+module Sivel2Gen
+  module Admin
+    class MaternidadesController < Sivel2Gen::Admin::BasicasController
+      before_action :set_maternidad, only: [:show, :edit, :update, :destroy]
+      load_and_authorize_resource class: Sivel2Gen::Maternidad
+
+      def clase 
+        "Sivel2Gen::Maternidad"
+      end
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_maternidad
+        @basica = Sivel2Gen::Maternidad.find(params[:id])
+      end
+
+      def genclase
+        return 'F';
+      end
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def maternidad_params
+        params.require(:maternidad).permit(*atributos_form)
+      end
+
+    end
+  end
+end
