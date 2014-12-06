5c169638508e4de94d2bd4db4f8ca9ca24dfa612
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-12-06 12:05:15 -0500

    actualizo con rails app:rails:update

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,15 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "sivel2_gen"
+
+require 'sivel2_gen'
 
 module Dummy
   class Application < Rails::Application
@@ -19,19 +16,18 @@ module Dummy
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Bogota'
+    config.time_zone = 'America/Bogota'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.active_record.schema_format = :sql
 
+    # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.active_record.schema_format = :sql
     FactoryGirl.definition_file_paths << Pathname.new("../factories")
     FactoryGirl.definition_file_paths.uniq!
     FactoryGirl.find_definitions
-
   end
 end
-
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
   devise_scope :usuario do
     get 'sign_out' => 'devise/sessions#destroy'
   end
@@ -10,8 +11,6 @@ Rails.application.routes.draw do
     put 'usuarios/:id' => 'devise/registrations#update', 
       :as => 'registro_usuario'            
   end
-  resources :usuarios, path_names: { new: 'nuevo', edit: 'edita' } 
-
-
+  resources :usuarios, path_names: { new: 'nuevo', edit: 'edita' }  
   mount Sivel2Gen::Engine, at: "sivel2_gen"
 end
