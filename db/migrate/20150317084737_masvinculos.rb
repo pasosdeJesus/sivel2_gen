0f0818d17f2378ad77eedf7ef95ba0416bd29a0f
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-03-17 09:01:37 -0500

    Migracion disminuida antes de propagar

--- db/migrate/20150317084737_masvinculos.rb
+++ db/migrate/20150317084737_masvinculos.rb
@@ -1,14 +1,13 @@
 class Masvinculos < ActiveRecord::Migration
   def up
     execute <<-SQL
-INSERT INTO sivel2_gen_vinculoestado (id, nombre, fechacreacion, created_at) VALUES (41, 'JUNTA DE ACCIÓN COMUNAL', '2015-03-17', '2015-03-17');
 INSERT INTO sivel2_gen_vinculoestado (id, nombre, fechacreacion, created_at) VALUES (42, 'FUERZAS MILITARES', '2015-03-17', '2015-03-17');
 INSERT INTO sivel2_gen_vinculoestado (id, nombre, fechacreacion, created_at) VALUES (43, 'POLICIA', '2015-03-17', '2015-03-17');
     SQL
   end
   def down
     execute <<-SQL
-DELETE FROM sivel2_gen_vinculoestado WHERE id IN ('41', '42', '43');
+DELETE FROM sivel2_gen_vinculoestado WHERE id IN ('42', '43');
     SQL
   end
 end
