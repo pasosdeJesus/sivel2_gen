fcb88e877b7e68a63c904d86e2daee597fb2e256
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-07-16 16:31:31 -0500

    permite editar tabla basica fuente prensa

--- app/models/sip/fuenteprensa.rb
+++ app/models/sip/fuenteprensa.rb
@@ -7,7 +7,7 @@ module Sip
     include Sip::Concerns::Models::Fuenteprensa
 
     has_many :caso_fuenteprensa, foreign_key: "fuenteprensa_id", 
-      validate: true, dependent: :destroy, class_name: 'Sivel2Gen::Caso'
+      validate: true, dependent: :destroy, class_name: 'Sivel2Gen::CasoFuenteprensa'
     has_many :anexo, foreign_key: "fuenteprensa_id", validate: true, 
       class_name: 'Sivel2Gen::Anexo'
   end
