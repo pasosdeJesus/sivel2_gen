3f42ec18f982583e7508736f55e6e53d60e859ab
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-07-16 16:31:54 -0500

    moderniza manejo de tablas basicas al nuevo sip

--- app/models/sivel2_gen/ability.rb
+++ app/models/sivel2_gen/ability.rb
@@ -12,7 +12,7 @@ module Sivel2Gen
 
     ROLES = Sip::Ability::ROLES
 
-    BASICAS = Sip::Ability::TABLAS_SIP + [
+    BASICAS_PROPIAS = [
       ['Sivel2Gen', 'actividadoficio'],  
       ['Sivel2Gen', 'categoria'], 
       ['Sivel2Gen', 'escolaridad'], 
@@ -24,7 +24,7 @@ module Sivel2Gen
       ['Sivel2Gen', 'intervalo'],
       ['Sivel2Gen', 'maternidad'], 
       ['Sivel2Gen', 'organizacion'],
-      ['Sivel2Gen', 'pconsolidado'], 
+#      ['Sivel2Gen', 'pconsolidado'], 
       ['Sivel2Gen', 'presponsable'], 
       ['Sivel2Gen', 'profesion'], 
       ['Sivel2Gen', 'rangoedad'],
@@ -35,76 +35,29 @@ module Sivel2Gen
       ['Sivel2Gen', 'tviolencia']
     ]
 
-    @@tablasbasicas += [
-      ['Sivel2Gen', 'actividadoficio'],  
-      ['Sivel2Gen', 'categoria'], 
-      ['Sivel2Gen', 'escolaridad'], 
-      ['Sivel2Gen', 'estadocivil'], 
-      ['Sivel2Gen', 'etnia'], 
-      ['Sivel2Gen', 'filiacion'], 
-      ['Sivel2Gen', 'frontera'],
-      ['Sivel2Gen', 'iglesia'], 
-      ['Sivel2Gen', 'intervalo'],
-      ['Sivel2Gen', 'maternidad'], 
-      ['Sivel2Gen', 'organizacion'],
-      ['Sivel2Gen', 'pconsolidado'], 
-      ['Sivel2Gen', 'presponsable'], 
-      ['Sivel2Gen', 'profesion'], 
-      ['Sivel2Gen', 'rangoedad'],
-      ['Sivel2Gen', 'region'], 
-      ['Sivel2Gen', 'sectorsocial'], 
-      ['Sivel2Gen', 'supracategoria'], 
-      ['Sivel2Gen', 'vinculoestado'], 
-      ['Sivel2Gen', 'tviolencia']
-    ]
-
-    def self.tablasbasicas
-      @@tablasbasicas
-    end
-
-    # Tablas basicas cuya secuencia es de la forma tabla_id_seq  (convención rails)
-    BASICAS_SEQID = Sip::Ability::TABLAS_SIP_SEQID + [ 
-      ['Sivel2Gen', 'supracategoria']
-    ]
-    @@basicas_seq_con_id += [ 
-      ['Sivel2Gen', 'supracategoria']
-    ]
-
-    def self.basicas_seq_con_id
-      @@basicas_seq_con_id
-    end
+    @@tablasbasicas = Sip::Ability::BASICAS_PROPIAS + BASICAS_PROPIAS
 
     # Tablas básicas cuyo id no es autoincremental
-    @@basicas_id_noauto += [ 
+    BASICAS_ID_NOAUTO = [
       ['Sivel2Gen', 'categoria'], 
       ['Sivel2Gen', 'tviolencia'] 
     ]
 
-    def self.basicas_id_noauto
-      @@basicas_id_noauto
-    end
+    @@basicas_id_noauto = Sip::Ability::BASICAS_ID_NOAUTO + BASICAS_ID_NOAUTO 
 
     # Tablas no básicas pero que tienen índice
-    NOBASICAS_INDICE =  [
+    NOBASICAS_INDSEQID =  [
       ['Sivel2Gen', 'caso'], 
     ]
-    @@nobasicas_indice += NOBASICAS_INDICE
-    def self.nobasicas_indice
-      @@nobasicas_indice
-    end
 
 		# Tablas no básicas pero que tienen índice con secuencia id_seq
-    @@nobasicas_indice_seq_con_id = Sip::Ability::NOBASICAS_INDSEQID
-
-    def self.nobasicas_indice_seq_con_id
-      @@nobasicas_indice_seq_con_id
-    end
-
+    @@nobasicas_indice_seq_con_id = Sip::Ability::NOBASICAS_INDSEQID +
+      NOBASICAS_INDSEQID
 
     # Tablas básicas que deben volcarse primero --por ser requeridas 
     # por otras básicas
-    BASICAS_PRIO = Sip::Ability::BASICAS_PRIO + [
-      ['Sivel2Gen', 'pconsolidado'], 
+    BASICAS_PRIO = [
+#      ['Sivel2Gen', 'pconsolidado'], 
 			['Sivel2Gen', 'tviolencia'], 
       ['Sivel2Gen', 'supracategoria'],
       ['Sivel2Gen', 'intervalo'], 
@@ -113,40 +66,7 @@ module Sivel2Gen
 			['Sivel2Gen', 'sectorsocial'],
       ['Sivel2Gen', 'vinculoestado']
     ];
-    @@tablasbasicas_prio += [
-      ['Sivel2Gen', 'pconsolidado'], 
-			['Sivel2Gen', 'tviolencia'], 
-      ['Sivel2Gen', 'supracategoria'],
-      ['Sivel2Gen', 'intervalo'], 
-			['Sivel2Gen', 'filiacion'], 
-      ['Sivel2Gen', 'organizacion'], 
-			['Sivel2Gen', 'sectorsocial'],
-      ['Sivel2Gen', 'vinculoestado']
-    ];
-    def self.tablasbasicas_prio
-      @@tablasbasicas_prio
-    end
-
-    # Recibe una tabla básica como pareja [Modulo, clase] y retorna
-    # clase completa Modulo::Clase
-    def self.tb_clase(t)
-      if (t[0] != '') 
-        k = t[0] + '::' + t[1].capitalize
-      else
-        k = t[1].capitalize
-      end
-      k.constantize 
-    end
-
-    # Recibe una tabla básica como pareja [Modulo, clase] y retorna
-    # nombre de tabla modulo_clase
-    def self.tb_modelo(t)
-      if (t[0] != '') 
-        t[0].underscore.gsub(/\//, '_') + '_' + t[1]
-      else 
-        t[1]
-      end
-    end
+    @@tablasbasicas_prio = Sip::Ability::BASICAS_PRIO + BASICAS_PRIO
 
 
     # Ver documentacion de este metodo en app/models/ability de sip
@@ -185,5 +105,6 @@ module Sivel2Gen
         end
       end
     end
-  end
-end
+
+  end # class
+end   # module
