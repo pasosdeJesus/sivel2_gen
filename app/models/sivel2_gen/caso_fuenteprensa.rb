a28ce0c36c75d280e96337d3e449301bdce762be
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-07-16 16:32:28 -0500

    permite editar fuenteprensa

--- app/models/sivel2_gen/caso_fuenteprensa.rb
+++ app/models/sivel2_gen/caso_fuenteprensa.rb
@@ -4,7 +4,7 @@ module Sivel2Gen
     has_many :anexo, foreign_key: "id_anexo", validate: true, 
       class_name: 'Sivel2Gen::Anexo'
     belongs_to :fuenteprensa, foreign_key: "fuenteprensa_id", validate: true, 
-      class_name: 'Sivel2Gen::Ffrecuente'
+      class_name: 'Sip::Fuenteprensa'
     belongs_to :caso, foreign_key: "id_caso", validate: true, 
       class_name: 'Sivel2Gen::Caso'
   end
