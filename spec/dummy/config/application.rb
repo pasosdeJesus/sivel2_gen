3ee81e5a075ef108d8f0902e81b31d40a87ba385
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-07-16 16:33:31 -0500

    consola en red interna

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -29,5 +29,7 @@ module Dummy
     FactoryGirl.definition_file_paths << Pathname.new("../factories")
     FactoryGirl.definition_file_paths.uniq!
     FactoryGirl.find_definitions
+
+    config.web_console.whitelisted_ips = '192.168.0.0/16'
   end
 end
