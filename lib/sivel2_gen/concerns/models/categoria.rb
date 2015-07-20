007aea3ad09819f73fa4b3ea11a5cd4a34219588
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-07-19 22:45:50 -0500

    categoria mejorada

---
+++ lib/sivel2_gen/concerns/models/categoria.rb
@@ -0,0 +1,43 @@
+# encoding: UTF-8
+
+module Sivel2Gen
+  module Concerns
+    module Models
+      module Categoria
+        extend ActiveSupport::Concern
+        included do
+
+          belongs_to :supracategoria, foreign_key: "supracategoria_id", 
+            validate: true, class_name: 'Sivel2Gen::Supracategoria'
+
+          belongs_to :contada, foreign_key: "contadaen", 
+            class_name: 'Sivel2Gen::Categoria'
+
+          has_many :acto, foreign_key: "id_categoria", validate: true,
+            class_name: 'Sivel2Gen::Acto'
+#          has_many :actosjr, foreign_key: "id_categoria", validate: true,
+#            class_name: 'Sivel2Gen::Actosjr'
+#          has_many :casosjr, foreign_key: "categoriaref", validate: true,
+#            class_name: 'Sivel2Gen::Casosjr'
+
+          def presenta_nombre
+            sup = Sivel2Gen::Supracategoria.find(self.supracategoria_id)
+            self.nombre + " (" + sup.nombre + " / " + 
+              Sivel2Gen::Tviolencia.find(sup.id_tviolencia).nombre + ")"
+          end
+
+          def presenta(atr)
+            clf = clase_llave_foranea(atr)
+            if (atr.to_s == "tipocat")
+              h={C: "Colectivo", I: "Individual", O: "Otro"}
+              return h[self[atr.to_s].to_sym]
+            else
+              presenta_gen(atr)
+            end
+          end
+        end
+
+      end
+    end
+  end
+end
