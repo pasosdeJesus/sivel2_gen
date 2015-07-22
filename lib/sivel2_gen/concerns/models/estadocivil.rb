94f2f7fdf1347bd829e8a09fe53a754e462724f3
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-07-22 07:58:27 -0500

    Fuente de prensa

--- lib/sivel2_gen/concerns/models/estadocivil.rb
+++ lib/sivel2_gen/concerns/models/estadocivil.rb
@@ -1,4 +1,5 @@
 # encoding: UTF-8
+
 module Sivel2Gen
   module Concerns
     module Models
---
+++ lib/sivel2_gen/concerns/models/fuenteprensa.rb
@@ -0,0 +1,23 @@
+# encoding: UTF-8
+
+require 'sip/concerns/models/fuenteprensa'   
+
+module Sivel2Gen
+  module Concerns
+    module Models
+      module Fuenteprensa
+        extend ActiveSupport::Concern
+        include Sip::Concerns::Models::Fuenteprensa
+
+        included do
+          has_many :caso_fuenteprensa, foreign_key: "fuenteprensa_id", 
+            validate: true, dependent: :destroy, class_name: 'Sivel2Gen::CasoFuenteprensa'
+          has_many :anexocaso, foreign_key: "fuenteprensa_id", validate: true, 
+            class_name: 'Sivel2Gen::AnexoCaso'
+        end
+
+      end
+    end
+  end
+end
+
