ebfb6925f0e74479575fe95183ea86da9d5dcf66
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-07-24 19:58:19 -0500

    Patron

--- bin/gc.sh
+++ bin/gc.sh
@@ -2,17 +2,17 @@
 # Hace prueba de regresión y envia a github
 
 
-grep "^ *gem.*.sip.*,.*path:" Gemfile > /dev/null 2> /dev/null
+grep "^ *gem.*sip.*,.*path:" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
 	echo "Gemfile incluye un sip cableado al sistema de archivos"
 	exit 1;
 } fi;
-grep "^ *gem.*.debugger.*" Gemfile > /dev/null 2> /dev/null
+grep "^ *gem.*debugger" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
 	echo "Gemfile incluye debugger que heroku no quiere"
 	exit 1;
 } fi;
-grep "^ *gem.*.byebug.*" Gemfile > /dev/null 2> /dev/null
+grep "^ *gem.*byebug" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
 	echo "Gemfile incluye byebug que rbx de travis-ci no quiere"
 	exit 1;
