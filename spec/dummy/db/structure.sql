4eddd7c41776f52db73a1081d856c32cce08dd3f
Vladimir Tamara <vtamara@pasosdeJesus.org>
2015-08-26 08:47:54 -0500

    Renombra migracion

--- spec/dummy/db/structure.sql
+++ spec/dummy/db/structure.sql
@@ -3908,3 +3908,5 @@ INSERT INTO schema_migrations (version) VALUES ('20150803082520');
 
 INSERT INTO schema_migrations (version) VALUES ('20150809032138');
 
+INSERT INTO schema_migrations (version) VALUES ('20150826000000');
+
