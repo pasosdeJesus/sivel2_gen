6c056a5b37e831834368d64fb16fa614838c5d63
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2016-03-17 07:39:22 -0500

    Validaciones a rango de edad

--- app/models/sivel2_gen/rangoedad.rb
+++ app/models/sivel2_gen/rangoedad.rb
@@ -7,5 +7,10 @@ module Sivel2Gen
             validate: true, class_name: 'Sivel2Gen::RangoedadVictimacolectiva'
     has_many :victima, foreign_key: "id_rangoedad", validate: true, 
       class_name: 'Sivel2Gen::Victima'
+
+
+    validates :nombre, length: { maximum: 20}
+    validates :rango, length: { maximum: 20}
+    validates :observaciones, length: { maximum: 5000}
   end
 end
