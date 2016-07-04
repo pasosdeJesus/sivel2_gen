50168ee9b66d4a7505ef5cf92626d4c0e3798f12
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-07-04 04:42:31 -0500

    arregal rutas

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
       ruta = File.join(Rails.configuration.relative_url_root, 
                        'usuarios/sign_in')
       post ruta, to: 'devise/sessions#create'
+      get  ruta, to: 'devise/sessions#new'
     end
   end
 
