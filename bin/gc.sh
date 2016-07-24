9e682d35be1db6c9868bec07bc268160407a09b3
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-07-24 10:46:31 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -54,7 +54,10 @@ if (test "$?" != "0") then {
 
 b=`git branch | grep "^*" | sed -e  "s/^* //g"`
 git status -s
-git commit -a
+if (test "$MENSCONS" = "") then {
+	MENSCONS="Actualiza"
+} fi;
+git commit -m "$MENSCONS" -a
 git push origin ${b}
 if (test "$?" != "0") then {
 	echo "No pudo subirse el cambio a github";
