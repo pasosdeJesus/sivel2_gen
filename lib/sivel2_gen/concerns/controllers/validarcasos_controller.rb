515205dac82d4ca4e5cd6159e610cacd28e7de9c
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-08-11 10:33:00 -0500

    agrega columna usuario en validaciones

--- lib/sivel2_gen/concerns/controllers/validarcasos_controller.rb
+++ lib/sivel2_gen/concerns/controllers/validarcasos_controller.rb
@@ -8,9 +8,9 @@ module Sivel2Gen
 
         included do
 
-          def validacion_estandar(casos, titulo, where, atr = [:id, :fecha],
-                                  encabezado = ['Código', 'Fecha del Hecho'])
-            res = casos.where(where).select(atr)
+          def validacion_estandar(casos, titulo, where, atr = [:id, :fecha, :nusuario],
+                                  encabezado = ['Código', 'Fecha del Hecho', 'Usuario'])
+            res = casos.joins('INNER JOIN sivel2_gen_iniciador ON sivel2_gen_iniciador.id_caso=id').where(where).select(atr)
             arr = ActiveRecord::Base.connection.select_all(res.to_sql)
             @validaciones << { 
               titulo: titulo,
@@ -59,7 +59,30 @@ module Sivel2Gen
             valida_sinmemo
           end
 
+          def crear_vista_iniciador
+            ActiveRecord::Base.connection.execute("
+       CREATE OR REPLACE VIEW sivel2_gen_iniciador AS (
+         SELECT s3.id_caso, s3.fechainicio, s3.id_usuario, usuario.nusuario 
+         FROM usuario, (SELECT s2.id_caso,
+            s2.fechainicio,
+            min(s2.id_usuario) AS id_usuario
+           FROM sivel2_gen_caso_usuario s2,
+            ( SELECT f1.id_caso,
+                    min(f1.fechainicio) AS m
+                   FROM sivel2_gen_caso_usuario f1
+                  GROUP BY f1.id_caso) c
+          WHERE s2.id_caso = c.id_caso 
+            AND s2.fechainicio = c.m
+          GROUP BY s2.id_caso, 
+            s2.fechainicio
+          ORDER BY s2.id_caso, 
+            s2.fechainicio
+          ) s3 
+          WHERE usuario.id=s3.id_usuario); ");
+          end
+
           def validar_interno
+            crear_vista_iniciador
             validar_sivel2_gen
           end
 
