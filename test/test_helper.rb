9fa71332f392262f2bf9f4a46881dd59cec42be7
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-03-12 06:52:46 -0500

    Pasando pruebas de rspec a minitest

---
+++ test/test_helper.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+ENV['RAILS_ENV'] ||= 'test'
+require_relative 'dummy/config/environment'
+require 'rails/test_help'
+
+# Usuario para ingresar y hacer pruebas
+PRUEBA_USUARIO = {
+  nusuario: "admin",
+  password: "sjrven123",
+  nombre: "admin",
+  descripcion: "admin",
+  rol: 1,
+  idioma: "es_CO",
+  email: "usuario1@localhost",
+  encrypted_password: '$2a$10$uMAciEcJuUXDnpelfSH6He7BxW0yBeq6VMemlWc5xEl6NZRDYVA3G',
+  sign_in_count: 0,
+  fechacreacion: "2014-08-05",
+  fechadeshabilitacion: nil,
+  oficina_id: nil
+}
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
