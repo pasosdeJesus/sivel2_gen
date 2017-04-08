b3935df6fb08b4e302b4102f04bf09f5f74cc460
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-04-08 15:37:44 -0500

    Actualiza

--- app/models/sivel2_gen/usuario.rb
+++ app/models/sivel2_gen/usuario.rb
@@ -8,8 +8,5 @@ module Sivel2Gen
     has_many :caso_etiqueta, foreign_key: "id_usuario", validate: true,
       class_name: 'Sivel2Gen::CasoEtiqueta'
 
-    belongs_to :oficina, foreign_key: "oficina_id", validate: true,
-      class_name: 'Sip::Oficina'
-
   end
 end
