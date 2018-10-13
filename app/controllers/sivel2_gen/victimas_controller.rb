9d4dd3b2d294bcaf146a6aa722d72fc9e3440608
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-10-13 05:35:00 -0500

    Actualiza

--- app/controllers/sivel2_gen/victimas_controller.rb
+++ app/controllers/sivel2_gen/victimas_controller.rb
@@ -1,46 +1,9 @@
 # encoding: UTF-8
-require 'date'
-module Sivel2Gen
-  class VictimasController < ApplicationController
-    load_and_authorize_resource class: Sivel2Gen::Victima
 
-    # Crea un nuevo registro para el caso que recibe por parametro 
-    # params[:caso_id].  Pone valores simples en los campos requeridos
-    def nuevo
-      if params[:caso_id]
-        @persona = Sip::Persona.new
-        @victima = Victima.new
-        @persona.nombres = 'N'
-        @persona.apellidos = 'N'
-        @persona.sexo = 'S'
-        if !@persona.save
-          respond_to do |format|
-            format.html { render inline: 'No pudo crear persona' }
-          end
-          return
-        end
-        @victima.id_caso = params[:caso_id]
-        @victima.id_persona = @persona.id
-        if @victima.save
-          respond_to do |format|
-            format.js { render json: {'victima' => @victima.id.to_s,
-                                      'persona' => @persona.id.to_s} }
-            format.json { render json: {'victima' => @victima.id.to_s,
-                                        'persona' => @persona.id.to_s}, status: :created }
-            format.html { render json: {'victima' => @victima.id.to_s,
-                                        'persona' => @persona.id.to_s} }
-          end
-        else
-          respond_to do |format|
-            format.html { render action: "error" }
-            format.json { render json: @victima.errors, status: :unprocessable_entity }
-          end
-        end
-      else
-        respond_to do |format|
-          format.html { render inline: 'Falta identificacion del caso' }
-        end
-      end
-    end
+require 'sivel2_gen/concerns/controllers/victimas_controller'
+
+module Sivel2Gen
+  class VictimasController < Heb412Gen::ModelosController
+    include Sivel2Gen::Concerns::Controllers::VictimasController
   end
 end
