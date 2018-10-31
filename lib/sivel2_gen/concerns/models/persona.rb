51403224a60f8bc1172c203b727a92e0c14b0fc7
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-10-30 21:12:21 -0500

    refina modelo persona

--- lib/sivel2_gen/concerns/models/persona.rb
+++ lib/sivel2_gen/concerns/models/persona.rb
@@ -13,8 +13,6 @@ module Sivel2Gen
 
           has_many :acto, foreign_key: "id_persona", validate: true,
             class_name: "Sivel2Gen::Acto"
-          has_many :antecedente_victima, foreign_key: "id_persona", 
-            validate: true, class_name: "Sivel2Gen::AntecedenteVictima"
           has_many :victima, foreign_key: "id_persona", validate: true,
             class_name: "Sivel2Gen::Victima"
           has_many :casos, :through => :victimas
