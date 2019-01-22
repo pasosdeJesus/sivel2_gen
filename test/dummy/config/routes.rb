31ae3cc8ebc1b7199db13dd76f1e86fc2fa8da46
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-01-22 07:24:56 -0500

    Filtro avanzado por sexo

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
 
     root 'sivel2_gen/hogar#index'
   end
-  mount Sivel2Gen::Engine, at: "/", as: "sivel2_gen"
-  mount Heb412Gen::Engine, at: "/", as: "heb412_gen"
-  mount Sip::Engine, at: "/", as: "sip"
+  mount Sivel2Gen::Engine, at: "/sivel2", as: "sivel2_gen"
+  mount Heb412Gen::Engine, at: "/sivel2", as: "heb412_gen"
+  mount Sip::Engine, at: "/sivel2", as: "sip"
 end
