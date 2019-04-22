0df059121d4a29c05f26d951b0522ae88658bad3
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-04-22 03:45:33 -0500

    Refactoriza fuentesprensa_controller para usar ActiveSupport::Concerns

--- app/controllers/sivel2_gen/fuentesprensa_controller.rb
+++ app/controllers/sivel2_gen/fuentesprensa_controller.rb
@@ -1,37 +1,8 @@
-# encoding: UTF-8
-require 'date'
+# encoding: UTF-8 
+require 'sivel2_gen/concerns/controllers/fuentesprensa_controller'
 
 module Sivel2Gen
-  class FuentesprensaController < ApplicationController
-    load_and_authorize_resource class: Sivel2Gen::Caso
-    
-    # Responde a requerimiento AJAX generado por cocoon creando una
-    # nueva fuente de prensa para el caso que recibe por parametro 
-    # params[:caso_id].  Pone valores simples en los campos requeridos
-    def nuevo
-      if !params[:caso_id].nil?
-        @fprensa = CasoFuenteprensa.new
-        @fprensa.id_caso = params[:caso_id]
-        @fprensa.fuenteprensa_id = 0
-        @fprensa.fecha = Date.today
-        if @fprensa.save
-          respond_to do |format|
-            format.js { render text: @fprensa.id.to_s }
-            format.json { render json: @fprensa.id.to_s, status: :created }
-            format.html { render inline: @fprensa.id.to_s }
-          end
-        else
-          respond_to do |format|
-            format.html { render action: "error" }
-            format.json { render json: @fprensa.errors, status: :unprocessable_entity }
-          end
-        end
-      else
-        respond_to do |format|
-          format.html { render inline: 'Falta identificacion del caso' }
-        end
-      end
-    end # def nuevo
-
-  end # class FuentesprensaController
-end # module Sivel2Gen
+  class FuentesprensaController < Heb412Gen::ModelosController
+    include Sivel2Gen::Concerns::Controllers::FuentesprensaController
+  end
+end
---
+++ lib/sivel2_gen/concerns/controllers/fuentesprensa_controller.rb
@@ -0,0 +1,49 @@
+# encoding: UTF-8
+
+require 'date'
+
+module Sivel2Gen
+  module Concerns
+    module Controllers
+      module FuentesprensaController
+        extend ActiveSupport::Concern
+
+        included do
+          include ActionView::Helpers::AssetUrlHelper
+
+          load_and_authorize_resource class: Sivel2Gen::Caso
+
+          # Responde a requerimiento AJAX generado por cocoon creando una
+          # nueva fuente de prensa para el caso que recibe por parametro 
+          # params[:caso_id].  Pone valores simples en los campos requeridos
+          def nuevo
+            if !params[:caso_id].nil?
+              @fprensa = CasoFuenteprensa.new
+              @fprensa.id_caso = params[:caso_id]
+              @fprensa.fuenteprensa_id = 0
+              @fprensa.fecha = Date.today
+              if @fprensa.save
+                respond_to do |format|
+                  format.js { render text: @fprensa.id.to_s }
+                  format.json { render json: @fprensa.id.to_s, status: :created }
+                  format.html { render inline: @fprensa.id.to_s }
+                end
+              else
+                respond_to do |format|
+                  format.html { render action: "error" }
+                  format.json { render json: @fprensa.errors, status: :unprocessable_entity }
+                end
+              end
+            else
+              respond_to do |format|
+                format.html { render inline: 'Falta identificacion del caso' }
+              end
+            end
+          end # def nuevo
+
+        end # included
+
+      end 
+    end
+  end
+end
