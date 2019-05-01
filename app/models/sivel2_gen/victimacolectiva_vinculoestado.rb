6bd2552503edd0f26eeaf9a9679989f626495dcd
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-05-01 13:11:20 -0500

    Cambia nombre

--- app/models/sivel2_gen/victimacolectiva_vinculoestado.rb
+++ app/models/sivel2_gen/victimacolectiva_vinculoestado.rb
@@ -3,7 +3,7 @@
 require 'sivel2_gen/concerns/models/victimacolectiva_vinculoestado'
 
 module Sivel2Gen
-  class Victima < ActiveRecord::Base
+  class VictimacolectivaVinculoestado < ActiveRecord::Base
     include Sivel2Gen::Concerns::Models::VictimacolectivaVinculoestado
   end
 end
