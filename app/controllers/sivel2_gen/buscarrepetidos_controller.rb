a9bcc9569aa35396c30844d4aa4258c389d54c23
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-05-31 17:40:30 -0500

    Buscar repetidos

---
+++ app/controllers/sivel2_gen/buscarrepetidos_controller.rb
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+
+require 'sivel2_gen/concerns/controllers/buscarrepetidos_controller'
+
+module Sivel2Gen
+  class BuscarrepetidosController < ApplicationController
+
+    include Sivel2Gen::Concerns::Controllers::BuscarrepetidosController
+
+  end
+end
---
+++ app/models/sivel2_gen/buscarrepetido.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+
+require 'sivel2_gen/concerns/models/buscarrepetido'
+
+module Sivel2Gen
+  class Buscarrepetido
+    include Sivel2Gen::Concerns::Models::Buscarrepetido
+  end
+end
