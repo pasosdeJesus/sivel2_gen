bc8b14d16f9464ae34eefaf7c80b6b807dd06fca
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-05-31 17:38:14 -0500

    Buscar repetidos

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Sivel2Gen::Engine.routes.draw do
   patch "/actoscolectivos/agregar" => 'actoscolectivos#agregar'
   get "/actoscolectivos/eliminar" => 'actoscolectivos#eliminar'
   get '/acercade' => 'hogar#acercade'
+  get "/buscarrepetidos" => 'buscarrepetidos#reportar', as: :buscarrepetidos
+  post "/buscarrepetidos" => 'buscarrepetidos#reportar', as: :envia_buscarrepetidos
   get "/casofotras/nuevo", 'casofotras#nuevo'
   get "/casos/busca" => 'casos#busca'
   get '/casos/lista' => 'casos#lista'
