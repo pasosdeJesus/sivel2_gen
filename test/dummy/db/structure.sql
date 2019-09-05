be253fdc7a882728ffaf5bf915f98b85b76b4e58
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-09-05 08:06:32 -0500

    Actualiza

--- test/dummy/db/structure.sql
+++ test/dummy/db/structure.sql
@@ -2865,44 +2865,6 @@ CREATE TABLE public.sivel2_gen_vinculoestado (
 
 
 --
--- Name: sivel2_gen_visual; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.sivel2_gen_visual (
-    id bigint NOT NULL,
-    nombre character varying(500) NOT NULL,
-    observaciones character varying(5000),
-    nav_ini character varying,
-    nav_fin character varying,
-    nav_fuente character varying,
-    fondo_lista character varying,
-    fechacreacion date NOT NULL,
-    fechadeshabilitacion date,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
-);
-
-
---
--- Name: sivel2_gen_visual_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.sivel2_gen_visual_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: sivel2_gen_visual_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.sivel2_gen_visual_id_seq OWNED BY public.sivel2_gen_visual.id;
-
-
---
 -- Name: vvictimasoundexesp; Type: MATERIALIZED VIEW; Schema: public; Owner: -
 --
 
@@ -3106,13 +3068,6 @@ ALTER TABLE ONLY public.sivel2_gen_combatiente ALTER COLUMN id SET DEFAULT nextv
 --
 
 ALTER TABLE ONLY public.sivel2_gen_resagresion ALTER COLUMN id SET DEFAULT nextval('public.sivel2_gen_resagresion_id_seq'::regclass);
-
-
---
--- Name: sivel2_gen_visual id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.sivel2_gen_visual ALTER COLUMN id SET DEFAULT nextval('public.sivel2_gen_visual_id_seq'::regclass);
 
 
 --
@@ -3825,14 +3780,6 @@ ALTER TABLE ONLY public.sivel2_gen_victimacolectiva
 
 ALTER TABLE ONLY public.sivel2_gen_victimacolectiva
     ADD CONSTRAINT sivel2_gen_victimacolectiva_pkey PRIMARY KEY (id);
-
-
---
--- Name: sivel2_gen_visual sivel2_gen_visual_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.sivel2_gen_visual
-    ADD CONSTRAINT sivel2_gen_visual_pkey PRIMARY KEY (id);
 
 
 --
@@ -5227,7 +5174,6 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20190430112229'),
 ('20190605143420'),
 ('20190612111043'),
-('20190618135558'),
 ('20190618135559'),
 ('20190625140232'),
 ('20190703044126'),
