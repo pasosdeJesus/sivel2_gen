a61d1b112eb2207f61e6a453d5b47c7075066ad4
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-11-12 17:20:17 -0500

    Autocompleta

--- test/dummy/app/javascript/packs/application.js
+++ test/dummy/app/javascript/packs/application.js
@@ -31,6 +31,7 @@ import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
 import 'jquery-ui'
+import 'jquery-ui/ui/widgets/autocomplete'
 import 'pick-a-color'
 import tinycolor from 'tinycolor2'
 
