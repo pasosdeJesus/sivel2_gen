4c3c0c5c0882b1dd98ce5270091c7fbe3554bb2a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-03-03 16:13:04 -0500

    Mejora migracion

--- db/migrate/20200224134339_ref_vista_conscas_tras_ag_fecha.rb
+++ db/migrate/20200224134339_ref_vista_conscas_tras_ag_fecha.rb
@@ -1,12 +1,12 @@
 class RefVistaConscasTrasAgFecha < ActiveRecord::Migration[6.0]
   def up
     execute <<-SQL
-      DROP MATERIALIZED VIEW sivel2_gen_conscaso CASCADE;
+      DROP MATERIALIZED VIEW IF EXISTS sivel2_gen_conscaso CASCADE;
     SQL
   end
   def down
     execute <<-SQL
-      DROP MATERIALIZED VIEW sivel2_gen_conscaso CASCADE;
+      DROP MATERIALIZED VIEW IF EXISTS sivel2_gen_conscaso CASCADE;
     SQL
   end
 end
