1d9d89c8b4cb80ca8b69614156adfc37ac4476c0
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-03-25 14:42:00 -0500

    Convencion

--- app/controllers/sivel2_gen/casos_controller.rb
+++ app/controllers/sivel2_gen/casos_controller.rb
@@ -5,10 +5,11 @@ require 'sivel2_gen/concerns/controllers/casos_controller'
 module Sivel2Gen
   class CasosController < Heb412Gen::ModelosController 
 
+    include Sivel2Gen::Concerns::Controllers::CasosController
+
     before_action :set_caso, only: [:show, :edit, :update, :destroy]
     load_and_authorize_resource class: Sivel2Gen::Caso, 
-      except: [:index, :show]
-    include Sivel2Gen::Concerns::Controllers::CasosController
+      except: [:index, :show, :mapaosm]
 
   end
 end
