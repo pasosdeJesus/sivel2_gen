2f11f764cc85687dc794cae65211a13d227604e4
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-03-26 05:33:46 -0500

    Mejora para #91

--- db/migrate/20200320152017_crea_contextovictima.rb
+++ db/migrate/20200320152017_crea_contextovictima.rb
@@ -8,6 +8,11 @@ class CreaContextovictima < ActiveRecord::Migration[6.0]
       t.timestamp :created_at, null: false
       t.timestamp :updated_at, null: false
     end
+    execute <<-SQL
+    INSERT INTO sivel2_gen_contextovictima (id, nombre, 
+      fechacreacion, created_at, updated_at) VALUES (1, 
+      'FALSO POSITIVO', '2020-03-26', '2020-03-26', '2020-03-26');
+    SQL
   end
 
   def down
