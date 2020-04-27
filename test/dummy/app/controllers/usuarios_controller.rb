103169ca677294bff0d3825866804c857bd4a1db
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-04-27 04:09:51 -0500

    Closes #117

--- test/dummy/app/controllers/usuarios_controller.rb
+++ test/dummy/app/controllers/usuarios_controller.rb
@@ -2,6 +2,10 @@
 
 require 'sip/concerns/controllers/usuarios_controller'
 
-class UsuariosController < Sip::ModelosController
+class UsuariosController < Heb412Gen::ModelosController
   include Sip::Concerns::Controllers::UsuariosController
+
+  def vistas_manejadas
+    ['Usuario']
+  end
 end
