dc7b4e8cd88ae69c51941e405dbfd859520cdb49
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-04-30 05:51:38 -0500

    Convierte caso_usuario a bitacora

---
+++ db/migrate/20200430101709_convierte_caso_usuario_a_bitacora.rb
@@ -0,0 +1,18 @@
+class ConvierteCasoUsuarioABitacora < ActiveRecord::Migration[6.0]
+  def up
+    execute <<-SQL
+      INSERT INTO sip_bitacora (fecha, usuario_id, modelo, modelo_id,
+        operacion, detalle, created_at, updated_at)
+      (SELECT fechainicio, id_usuario, 'Sivel2Gen::Caso', id_caso, 
+        'tocar', '{"origen": "caso_usuario"}', NOW(), NOW() 
+        FROM sivel2_gen_caso_usuario);
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DELETE FROM sip_bitacora WHERE operacion='tocar' AND
+        (detalle->>'origen') = 'caso_usuario';
+    SQL
+  end
+end
