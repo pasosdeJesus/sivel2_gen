b6e5e7902c0ca850eda197a3f11496fc9e63979e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-05-27 06:01:43 -0500

    Simplifica

--- lib/sivel2_gen/concerns/models/filiacion.rb
+++ lib/sivel2_gen/concerns/models/filiacion.rb
@@ -13,7 +13,6 @@ module Sivel2Gen
                 has_and_belongs_to_many :victimacolectiva, 
                   foreign_key: 'id_filiacion', 
                   class: 'Sivel2Gen::Victimacolectiva',
-                  association_foreign_key: 'victimacolectiva_id',
                   join_table: 'sivel2_gen_filiacion_victimacolectiva'
 
                 has_many :victima, foreign_key: 'id_filiacion', 
