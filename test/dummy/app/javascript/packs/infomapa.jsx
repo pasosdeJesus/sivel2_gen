6c1c47a3973af4ef1268b9e604ab083c40211e99
alejocruzrcc <alejocruzzz@gmail.com>
2020-07-08 11:10:36 -0500

    Agrega react y funciona prueba hola mundo

---
+++ test/dummy/app/javascript/packs/infomapa.jsx
@@ -0,0 +1,12 @@
+// Mapa con casos e indicadores, fuentes de Covid19.health
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+//import HelloWorld from '../components/HelloWorld'
+
+console.log("entro a react")
+const Appo  = ()=>{
+  return <h1> Hola desde appo </h1>
+}
+render(<Appo />, document.getElementById('root'))
