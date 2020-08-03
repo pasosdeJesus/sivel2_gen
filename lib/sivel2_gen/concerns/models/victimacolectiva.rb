ef4769c803249a0782d3bc9775216499d908beb3
alejocruzrcc <alejocruzzz@gmail.com>
2020-08-03 04:22:28 -0500

    Mejoraimporta (#203)
    * agrega validacion en victimacolectiva obs
    
    * arregla fallas en importacion
    
    * elimina innecesario

--- lib/sivel2_gen/concerns/models/victimacolectiva.rb
+++ lib/sivel2_gen/concerns/models/victimacolectiva.rb
@@ -93,62 +93,74 @@ module Sivel2Gen
               else
                 array_obs = g['observaciones']
               end
-              array_obs.each do |ob|
-                ele = ob.split(/\_([^_]*)$/)
-                case ele[0]
-                when 'personasaprox'
-                  self.personasaprox = ele[1]
-                when 'organizacion_armada'
-                  self.personasaprox = Sivel2Gen::Presponsable.where(nombre: ele[1]).ids[0]
-                when 'filiacion'
-                  if Sivel2Gen::Filiacion.where(nombre: ele[1]).count == 1
-                    fv = Sivel2Gen::FiliacionVictimacolectiva.new
-                    fv.id_filiacion = Sivel2Gen::Filiacion.where(nombre: ele[1]).ids[0]
-                    fv.victimacolectiva_id = self.id
-                    fv.save!
+              if array_obs
+                array_obs.each do |ob|
+                  ele = ob.split(/\_([^_]*)$/)
+                  case ele[0]
+                  when 'personasaprox'
+                    self.personasaprox = ele[1]
+                  when 'organizacion_armada'
+                    self.personasaprox = Sivel2Gen::Presponsable.where(nombre: ele[1]).ids[0]
+                  when 'filiacion'
+                    if Sivel2Gen::Filiacion.where(nombre: ele[1]).count == 1
+                      fv = Sivel2Gen::FiliacionVictimacolectiva.new
+                      fv.id_filiacion = Sivel2Gen::Filiacion.where(nombre: ele[1]).ids[0]
+                      fv.victimacolectiva_id = self.id
+                      fv.save!
+                    end
+                  when 'organizacion'
+                    if Sivel2Gen::Organizacion.where(nombre: ele[1]).count == 1
+                      org = Sivel2Gen::OrganizacionVictimacolectiva.new
+                      org.id_organizacion = Sivel2Gen::Organizacion.where(nombre: ele[1]).ids[0]
+                      org.victimacolectiva_id = self.id
+                      org.save!
+                    else
+                      menserror << "En la tabla básica Organización no está " +
+                        "'#{ele[1]}'"
+                    end
+                  when 'profesion'
+                    if Sivel2Gen::Profesion.where(nombre: ele[1]).count == 1
+                      pr = Sivel2Gen::ProfesionVictimacolectiva.new
+                      pr.id_profesion = Sivel2Gen::Profesion.where(nombre: ele[1]).ids[0]
+                      pr.victimacolectiva_id = self.id
+                      pr.save!
+                    else
+                      menserror << "En la tabla básica Profesión no está " +
+                        "'#{ele[1]}'"
+                    end
+                  when 'rangoedad'
+                    if Sivel2Gen::Rangoedad.where(nombre: ele[1]).count ==1
+                      re = Sivel2Gen::RangoedadVictimacolectiva.new
+                      re.id_rangoedad = Sivel2Gen::Rangoedad.where(nombre: ele[1]).ids[0]
+                      re.victimacolectiva_id = self.id
+                      re.save!
+                    else
+                      menserror << "En la tabla básica Rango Edad no está " +
+                        "'#{ele[1]}'"
+                    end
+                  when 'sectorsocial'
+                    if Sivel2Gen::Sectorsocial.where(nombre: ele[1]).count == 1
+                      ss = Sivel2Gen::SectorsocialVictimacolectiva.new
+                      ss.id_sectorsocial = Sivel2Gen::Sectorsocial.where(nombre: ele[1]).ids[0]
+                      ss.victimacolectiva_id = self.id
+                      ss.save!
+                    else
+                      menserror << "En la tabla básica Sector Social no está " +
+                        "'#{ele[1]}'"
+                    end
+                  when 'vinculoestado'
+                    if Sivel2Gen::Vinculoestado.where(nombre: ele[1].split("\;")).count == 1
+                      ve = Sivel2Gen::VictimacolectivaVinculoestado.new
+                      ve.id_vinculoestado = Sivel2Gen::Vinculoestado.where(nombre: ele[1].split("\;")).ids[0]
+                      ve.victimacolectiva_id = self.id
+                      ve.save!
+                    else
+                      menserror << "En la tabla básica Vínculo Estado no está " +
+                        "'#{ele[1].split("\;")}'"
+                    end
                   end
-                when 'organizacion'
-                  if Sivel2Gen::Organizacion.where(nombre: ele[1]).count == 1
-                    org = Sivel2Gen::OrganizacionVictimacolectiva.new
-                    org.id_organizacion = Sivel2Gen::Organizacion.where(nombre: ele[1]).ids[0]
-                    org.victimacolectiva_id = self.id
-                    org.save!
-                  else
-                    menserror << "En la tabla básica Organización no está " +
-                      "'#{ele[1]}'"
-                  end
-                when 'profesion'
-                  if Sivel2Gen::Profesion.where(nombre: ele[1]).count == 1
-                    pr = Sivel2Gen::ProfesionVictimacolectiva.new
-                    pr.id_profesion = Sivel2Gen::Profesion.where(nombre: ele[1]).ids[0]
-                    pr.victimacolectiva_id = self.id
-                    pr.save!
-                  else
-                    menserror << "En la tabla básica Profesión no está " +
-                      "'#{ele[1]}'"
-                  end
-                when 'rangoedad'
-                  re = Sivel2Gen::RangoedadVictimacolectiva.new
-                  re.id_rangoedad = Sivel2Gen::Rangoedad.where(nombre: ele[1]).ids[0]
-                  re.victimacolectiva_id = self.id
-                  re.save!
-                when 'sectorsocial'
-                  if Sivel2Gen::Sectorsocial.where(nombre: ele[1]).count == 1
-                    ss = Sivel2Gen::SectorsocialVictimacolectiva.new
-                    ss.id_sectorsocial = Sivel2Gen::Sectorsocial.where(nombre: ele[1]).ids[0]
-                    ss.victimacolectiva_id = self.id
-                    ss.save!
-                  else
-                    menserror << "En la tabla básica Sector Social no está " +
-                      "'#{ele[1]}'"
-                  end
-                when 'vinculoestado'
-                  ve = Sivel2Gen::VictimacolectivaVinculoestado.new
-                  ve.id_vinculoestado = Sivel2Gen::Vinculoestado.where(nombre: ele[1]).ids[0]
-                  ve.victimacolectiva_id = self.id
-                  ve.save!
-                end
-              end  
+                end  
+              end
             end
           end
         end
