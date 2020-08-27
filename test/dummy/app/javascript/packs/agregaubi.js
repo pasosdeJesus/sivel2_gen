c17822c3b0f79c52b391f6098e6487a6f26c581c
alejocruzrcc <alejocruzzz@gmail.com>
2020-08-27 09:27:02 -0500

    Reactreactivo (#228)
    * agrega ejemplo de react para formularios
    
    * agrega ejemplo de react para formularios
    
    * imprime el estado

---
+++ test/dummy/app/javascript/packs/agregaubi.js
@@ -0,0 +1,8 @@
+// Mapa con casos e indicadores, fuentes de Covid19.health
+
+ import React from 'react'
+ import ReactDOM from 'react-dom'
+ import { render } from 'react-dom'
+ import UbiFormu from '../components/UbiFormu'
+
+ render( <UbiFormu />, document.getElementById('root') )
