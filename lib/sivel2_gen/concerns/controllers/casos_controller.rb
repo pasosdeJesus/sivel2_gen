f4e95eebcf2537d724e7ab45879d30885766418a
alejocruzrcc <alejocruzzz@gmail.com>
2020-10-20 11:19:59 -0500

    cambia a español api (#258)

--- lib/sivel2_gen/concerns/controllers/casos_controller.rb
+++ lib/sivel2_gen/concerns/controllers/casos_controller.rb
@@ -451,7 +451,7 @@ module Sivel2Gen
             fechasinicial = Sivel2Gen::Caso.all.order(fecha: :asc).pluck(:fecha).uniq
             fechafinal = params[:fechafin] ? params[:fechafin] : Date.today
             fechainicial = params[:fechaini] ? params[:fechaini] : fechasinicial[0]
-            sql = "select fecha, count(distinct sivel2_gen_caso.id), sip_departamento.nombre FROM sivel2_gen_caso LEFT JOIN sip_ubicacion ON sivel2_gen_caso.ubicacion_id = sip_ubicacion.id LEFT JOIN sip_departamento ON sip_ubicacion.id_departamento = sip_departamento.id WHERE sivel2_gen_caso.fecha BETWEEN '" + fechainicial.to_s + "' AND '" + fechafinal.to_s + "' group by 1,3 order by 1;"
+            sql = "select fecha, count(distinct sivel2_gen_caso.id) AS cuenta, sip_departamento.nombre FROM sivel2_gen_caso LEFT JOIN sip_ubicacion ON sivel2_gen_caso.ubicacion_id = sip_ubicacion.id LEFT JOIN sip_departamento ON sip_ubicacion.id_departamento = sip_departamento.id WHERE sivel2_gen_caso.fecha BETWEEN '" + fechainicial.to_s + "' AND '" + fechafinal.to_s + "' group by 1,3 order by 1;"
             array_cuentas = ActiveRecord::Base.connection.execute(sql)
             render 'cuenta.json', layout: 'application', locals: {casosc: array_cuentas}
           end
