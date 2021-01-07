83b07e381a7d895ed0332d8c1d08154e7a64bd4c
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-01-07 16:03:21 -0500

    Sincroniza con lo que habia en master

--- lib/sivel2_gen/concerns/models/caso_fotra.rb
+++ lib/sivel2_gen/concerns/models/caso_fotra.rb
@@ -22,7 +22,7 @@ module Sivel2Gen
           validates :caso, presence: true
           validates :nombre, presence: true
           validates :nombre, length: { maximum: 500 } 
-          validates :anotacion, length: { maximum: 200 } 
+          validates :anotacion, length: { maximum: 1024 } 
           validates :ubicacionfisica, length: { maximum: 1024 } 
           validates :tfuente, length: { maximum: 25 } 
           validates :nombre, uniqueness: { scope: [ :fecha, :id_caso ] }
