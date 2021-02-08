b88339161c553f8dc8c4a69ffedb126ec5d6c5f0
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-02-08 04:36:25 -0500

    Arregla c910

--- db/migrate/20210206191033_agrega_c910.rb
+++ db/migrate/20210206191033_agrega_c910.rb
@@ -1,13 +1,13 @@
 class AgregaC910 < ActiveRecord::Migration[6.0]
   def up
     execute <<-SQL
-      UPDATE sivel2_gen_categoria SET fechacreacion=NULL
+      UPDATE sivel2_gen_categoria SET fechadeshabilitacion=NULL
         WHERE id=910;
     SQL
   end
   def down
     execute <<-SQL
-      UPDATE sivel2_gen_categoria SET fechacreacion='2017-05-03'
+      UPDATE sivel2_gen_categoria SET fechadeshabilitacion='2017-05-03'
         WHERE id=910;
     SQL
   end
