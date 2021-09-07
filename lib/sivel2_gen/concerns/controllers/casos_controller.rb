a7d239902848282e8e4324acefea5408bc90f05b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-09-07 10:45:22 -0500

    Reorganiza

--- lib/sivel2_gen/concerns/controllers/casos_controller.rb
+++ lib/sivel2_gen/concerns/controllers/casos_controller.rb
@@ -225,33 +225,32 @@ module Sivel2Gen
 
             # Filtro
             if params && params[:filtro]
-#              if params[:filtro][:q] && params[:filtro][:q].length>0
-                # Simple
-#                q = params[:filtro][:q].gsub("-", " ")
-#                @conscaso = @conscaso.where(
-#                  "q @@ plainto_tsquery('spanish', unaccent(?))", q
-#                )
-#              else
-                # Avanzado
-                @conscaso = filtro_avanzado @conscaso, params[:filtro]
-                #puts @conscaso.to_sql; byebug
-                # Columnas por incluir
-                nincluir = []
-                for i in @incluir do
-                  s = 'inc_' + i
-                  if params[:filtro][s.to_sym] && params[:filtro][s.to_sym] == '1'
-                    nincluir.push(i)
-                  end
-#                end
-                @incluir = nincluir
-                # Cambiar Ordenamiento
-                if params[:filtro][:orden]
-                  @campoord = params[:filtro][:orden]
+              # if params[:filtro][:q] && params[:filtro][:q].length>0
+              # Simple
+              #   q = params[:filtro][:q].gsub("-", " ")
+              #   @conscaso = @conscaso.where(
+              #     "q @@ plainto_tsquery('spanish', unaccent(?))", q
+              #   )
+              # else
+              # Avanzado
+              @conscaso = filtro_avanzado @conscaso, params[:filtro]
+              #puts @conscaso.to_sql; byebug
+              # Columnas por incluir
+              nincluir = []
+              for i in @incluir do
+                s = 'inc_' + i
+                if params[:filtro][s.to_sym] && params[:filtro][s.to_sym] == '1'
+                  nincluir.push(i)
                 end
-                # Otros (puede cambiar consulta, @incluir o @campoord)
-                @conscaso = filtro_particular @conscaso, params[:filtro]
-                #puts @conscaso.to_sql; byebug
               end
+              @incluir = nincluir
+              # Cambiar Ordenamiento
+              if params[:filtro][:orden]
+                @campoord = params[:filtro][:orden]
+              end
+              # Otros (puede cambiar consulta, @incluir o @campoord)
+              @conscaso = filtro_particular @conscaso, params[:filtro]
+              #puts @conscaso.to_sql; byebug
             end
 
             # Ordenamiento y control de acceso
