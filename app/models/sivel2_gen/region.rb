4701049e670d170cc24b8e39f72379304e7998e3
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-09-22 10:06:39 -0500

    Modelo region modular

--- app/models/sivel2_gen/region.rb
+++ app/models/sivel2_gen/region.rb
@@ -1,22 +1,8 @@
-# encoding: UTF-8
+require 'sivel2_gen/concerns/models/region'
+
 module Sivel2Gen
   class Region < ActiveRecord::Base
-    include Sip::Basica
 
-    has_many :caso_region, foreign_key: "id_region", 
-      dependent: :delete_all, class_name: 'Sivel2Gen::CasoRegion'
-    has_many :caso, through: :caso_region, class_name: 'Sivel2Gen::Caso'
-
-    has_many :region_departamento, dependent: :delete_all,
-      class_name: 'Sivel2Gen::DepartamentoRegion',
-      foreign_key: 'region_id'
-    has_many :departamento, through: :region_departamento,
-      class_name: 'Sip::Departamento'
-
-    has_many :region_municipio, dependent: :delete_all,
-      class_name: 'Sivel2Gen::MunicipioRegion',
-      foreign_key: 'region_id'
-    has_many :municipio, through: :region_municipio,
-      class_name: 'Sip::Municipio'
+        include Sivel2Gen::Concerns::Models::Region
   end
 end
