03a49133ddffa45cee5670568c5640fcc690e8ca
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-09-24 09:24:21 -0500

    Evita duplicación en tablas asociativas

--- db/migrate/20210924022913_evita_repeticion_tablas_union.rb
+++ db/migrate/20210924022913_evita_repeticion_tablas_union.rb
@@ -1,14 +1,13 @@
 class EvitaRepeticionTablasUnion < ActiveRecord::Migration[6.1]
 
   LTD = [
-    ['antecedente', 'caso', 'id_', 'id_'], 
+    ['antecedente', 'caso', 'id_', 'id_'],
     ['antecedente', 'combatiente', 'id_', 'id_' ],
-    ['antecedente', 'victima', 'id_', 'id_']
-    ['antecedente', 'victimacolectiva', 'id_', '_id']
-    ['caso', 'contexto', 'id_', 'id_'], 
-    ['caso', 'region', 'id_', 'id_'], 
-    ['caso', 'respuestafor', '_id', '_id'], 
-    ['contextovictima', 'victima', '_id', '_id'], 
+    ['antecedente', 'victima', 'id_', 'id_'],
+    ['antecedente', 'victimacolectiva', 'id_', '_id'],
+    ['caso', 'contexto', 'id_', 'id_'],
+    ['caso', 'region', 'id_', 'id_'],
+    ['contextovictima', 'victima', '_id', '_id'],
     ['etnia', 'victimacolectiva', '_id', '_id'],
     ['filiacion', 'victimacolectiva', 'id_', '_id'],
     ['organizacion', 'victimacolectiva', 'id_', '_id'],
@@ -27,8 +26,8 @@ class EvitaRepeticionTablasUnion < ActiveRecord::Migration[6.1]
       c2= (td[3] == 'id_') ? "id_#{td[1]}" : "#{td[1]}_id"
       execute <<-EOF
         CREATE TABLE public.#{tt} (
-          #{c1} integer NOT NULL REFERENCES sivel2_gen_#{t[0]},
-          #{c2} integer NOT NULL REFERENCES sivel2_gen_#{t[1]},
+          #{c1} integer NOT NULL REFERENCES sivel2_gen_#{td[0]},
+          #{c2} integer NOT NULL REFERENCES sivel2_gen_#{td[1]},
           CONSTRAINT "#{t}_pkey1" PRIMARY KEY (#{c1}, #{c2})
         );
         INSERT INTO #{tt} (SELECT  DISTINCT #{c1}, #{c2} FROM #{t});
@@ -36,11 +35,30 @@ class EvitaRepeticionTablasUnion < ActiveRecord::Migration[6.1]
         ALTER TABLE #{tt} RENAME TO #{t};
       EOF
     end
+
+    execute <<-EOF
+      CREATE TABLE public.caso_respuestafor (
+        caso_id integer NOT NULL REFERENCES sivel2_gen_caso,
+        respuestafor_id integer NOT NULL REFERENCES mr519_gen_respuestafor,
+        CONSTRAINT "sivel2_gen_caso_respuestafor_pkey1" 
+        PRIMARY KEY (caso_id, respuestafor_id)
+      );
+      INSERT INTO caso_respuestafor (SELECT  DISTINCT caso_id, respuestafor_id 
+        FROM caso_respuestafor);
+      DROP TABLE sivel2_gen_caso_respuestafor;
+      ALTER TABLE caso_respuestafor RENAME TO sivel2_gen_caso_respuestafor;
+    EOF
   end
 
   def down
     # Devuelve a situación si llave primaria compuesta (pero ignora marcas de
     # tiempo de algunas tablas que tenían inutilmente)
+
+    execute <<-EOF
+      ALTER TABLE public.sivel2_gen_caso_respuestafor
+        DROP CONSTRAINT sivel2_gen_caso_respuestafor_pkey1;
+    EOF
+
     LTD.each do |td|
       t = "sivel2_gen_#{td[0]}_#{td[1]}"
       c1= (td[2] == 'id_') ? "id_#{td[0]}" : "#{td[0]}_id"
