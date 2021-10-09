796705b95fec1fdcb934ef56b2a4f8d664475798
Alejandro Cruz <alejocruzzz@gmail.com>
2021-10-09 13:05:24 -0500

    grafica de sexo totales y filtro depa funciona

--- app/controllers/sivel2_gen/fil23_gen/graficar_apexcharts_controller.rb
+++ app/controllers/sivel2_gen/fil23_gen/graficar_apexcharts_controller.rb
@@ -7,8 +7,9 @@ module Sivel2Gen
       def victimizaciones_individuales
         authorize! :contar, Sivel2Gen::Caso
         @vic_fechaini = params[:filtro] ? Sip::FormatoFechaHelper.fecha_local_estandar(params[:filtro][:fechaini]) : "1998-01-01"
-        @vic_fechafin = params[:filtro] ? Sip::FormatoFechaHelper.fecha_local_estandar(params[:filtro][:fechafin])
- : "2020-12-31"
+        @vic_fechafin = params[:filtro] ? Sip::FormatoFechaHelper.fecha_local_estandar(params[:filtro][:fechafin]) : "2020-12-31"
+
+        @vic_dep = params[:filtro] ? params[:filtro][:departamentos] : Sip::Departamento.habilitados.pluck(:id)
         @categorias = Sivel2Gen::Categoria.from(
           'sivel2_gen_categoria, sivel2_gen_supracategoria').
           where('sivel2_gen_supracategoria.id=sivel2_gen_categoria.supracategoria_id 
@@ -19,14 +20,15 @@ module Sivel2Gen
         def consulta(sexo)
           "select caso.fecha as fecha_caso, count(*) as total from cvt1 JOIN sivel2_gen_caso as caso ON caso.id=id_caso 
           JOIN sip_persona AS persona ON persona.id=id_persona
+          JOIN sip_ubicacion as ubi ON ubi.id=caso.ubicacion_id
           JOIN sivel2_gen_categoria AS categoria ON categoria.id=id_categoria
           WHERE persona.sexo ='#{sexo}' 
           AND caso.fecha >='" + @vic_fechaini + "'
           AND caso.fecha <='" + @vic_fechafin + "'
+          AND ubi.id_departamento IN (#{(@vic_dep - ['']).join(', ')})
           AND categoria.nombre IN (" + (@vic_categorias - ['']).map{|k| "'" + k + "'"}.join(', ') + ")
           GROUP BY fecha_caso ORDER BY fecha_caso;"
         end
-
         def consulta_totsex
           "select persona.sexo as sexo_persona, count(*) as total from cvt1
           JOIN sip_persona AS persona ON persona.id=id_persona 
