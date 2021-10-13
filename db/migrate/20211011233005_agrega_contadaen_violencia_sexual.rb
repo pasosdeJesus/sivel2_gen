bd9c396043ec23aa3f36ce404adc52358badca6a
alejocruzrcc <alejocruzzz@gmail.com>
2021-10-12 19:44:58 -0500

    R-464  F-486 cambia violación y agrega violencia sexual a categorías de violencia … (#488)
    * cambia violacion y agrega violenca sexual a categroias de violanecia sexual
    
    * cambio violaciones mal

---
+++ db/migrate/20211011233005_agrega_contadaen_violencia_sexual.rb
@@ -0,0 +1,26 @@
+class AgregaContadaenViolenciaSexual < ActiveRecord::Migration[6.1]
+  def up
+    categorias = ["ABORTO FORZADO", "ABUSO SEXUAL", "EMBARAZO FORZADO", "ESCLAVITUD SEXUAL", "ESTERILIZACIÓN FORZADA", "PROSTITUCIÓN FORZADA", "VIOLACIÓN"]
+    categorias.each do |cat|
+      [1, 2, 4, 5, 6, 8].each do |supra|
+        cat_violencia = Sivel2Gen::Categoria.where(nombre: 'VIOLENCIA SEXUAL').where(supracategoria_id: supra)[0]
+        execute <<-SQL
+          UPDATE sivel2_gen_categoria SET contadaen=#{cat_violencia.id} 
+            WHERE supracategoria_id=#{supra} AND nombre='#{cat}'; 
+        SQL
+      end
+    end
+  end
+
+  def down
+    categorias = ["ABORTO FORZADO", "ABUSO SEXUAL", "EMBARAZO FORZADO", "ESCLAVITUD SEXUAL", "ESTERILIZACIÓN FORZADA", "PROSTITUCIÓN FORZADA", "VIOLACIÓN"]
+    categorias.each do |cat|
+      [1, 2, 4, 5, 6, 8].each do |supra|
+        execute <<-SQL
+          UPDATE sivel2_gen_categoria SET contadaen=NULL 
+            WHERE supracategoria_id=#{supra} AND nombre='#{cat}'; 
+        SQL
+      end
+    end
+  end
+end
