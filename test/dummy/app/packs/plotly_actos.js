17147970540b3d009a2cbcc2f4f316c87c4ebb3e
Alejandro Cruz <alejocruzzz@gmail.com>
2021-10-26 10:15:41 -0500

    arregla formato y fechas y arregla filtro que no funcionaba bien

--- test/dummy/app/packs/plotly_actos.js
+++ test/dummy/app/packs/plotly_actos.js
@@ -243,8 +243,11 @@ function procesar_datos(filas, variable) {
   datosrEtiquetas = Object.keys(dicEtiquetas)
 
   seleccionmAsignarOpciones(selectorCategoria, datosrEtiquetas, true);
-  campoFechaini.value = datosrFechamin
-  campoFechafin.value = datosrFechamax
+  var fechaminLoc = sip_retorna_fecha_localizada(datosrFechamin, 'yyyy-mm-dd')
+
+  campoFechaini.value = sip_retorna_fecha_localizada(datosrFechamin, 'yyyy-mm-dd')
+
+  campoFechafin.value = sip_retorna_fecha_localizada(datosrFechamax, 'yyyy-mm-dd')
 
 
   /*var colores = {
@@ -255,9 +258,8 @@ function procesar_datos(filas, variable) {
 
   function actualizarGraficaFechas() {
     var opelegidas = seleccionmOpcionesElegidas(selectorCategoria)
-    var fechaini = campoFechaini.value
-    var fechafin = campoFechafin.value
-
+    var fechaini = sip_partes_fecha_localizada(campoFechaini.value, "dd/M/yyyy").join('-')
+    var fechafin = sip_partes_fecha_localizada(campoFechafin.value, "dd/M/yyyy").join('-')
     recalcularSeriesPresentadas(fechaini, fechafin, opelegidas);
   }
 
