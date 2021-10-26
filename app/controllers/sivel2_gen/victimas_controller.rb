51da770560b1496c3781e7f1af6bbbbccae44df6
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-10-26 05:31:32 -0500

    agrega end

--- app/controllers/sivel2_gen/victimas_controller.rb
+++ app/controllers/sivel2_gen/victimas_controller.rb
@@ -4,4 +4,5 @@ module Sivel2Gen
   class VictimasController < Heb412Gen::ModelosController
     load_and_authorize_resource class: Sivel2Gen::Victima
     include Sivel2Gen::Concerns::Controllers::VictimasController
+  end
 end
