59f0546778b26600bda219b38503276b05bdb315
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-10-30 04:03:20 -0500

    tooltip y orden importacion

--- test/dummy/app/packs/entrypoints/application.js
+++ test/dummy/app/packs/entrypoints/application.js
@@ -22,15 +22,16 @@ import Turbolinks from "turbolinks"
 Turbolinks.start()
 
 import $ from "expose-loader?exposes=$,jQuery!jquery";
+import 'jquery-ui'
+import 'jquery-ui/ui/widgets/autocomplete'
+import 'jquery-ui/ui/data'
+import 'jquery-ui/ui/focusable'
+import 'jquery-ui/ui/widgets/tooltip'
 import 'popper.js'              // Dialogos emergentes usados por bootstrap
 import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
 import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
-import 'jquery-ui'
-import 'jquery-ui/ui/widgets/autocomplete'
-import 'jquery-ui/ui/data'
-import 'jquery-ui/ui/focusable'
 
 // Apexcharts
 import ApexCharts from 'apexcharts'
