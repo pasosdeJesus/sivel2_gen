1afd76275e8596911a3818a7c271758297238c6b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-11-07 16:24:37 -0500

    Avanza titulo y subtitulo de mapa

--- app/controllers/sivel2_gen/mapadep_controller.rb
+++ app/controllers/sivel2_gen/mapadep_controller.rb
@@ -3,9 +3,35 @@ module Sivel2Gen
   class MapadepController < ApplicationController
 
     # Control de acceso no estándar en función
-    
-    def victimizaciones
 
+    def nomrango(fechaini, fechafin)
+      if fechaini > fechafin
+        return 'fechaini > fechafin'
+      end
+      if fechaini == fechafin
+        return fechaini.to_s
+      end
+      ai = "/#{fechaini.year}"
+      af = "/#{fechafin.year}"
+      if fechaini.year == fechafin.year
+        ai=''
+        af=" de #{fechaini.year}"
+      end
+      if fechaini.day == 1
+        mi = MES[fechaini.month]
+      else
+        mi = "#{fechaini.day}/#{fecahini.month}"
+      end
+      if fechafin.day == fechafin.end_of_month.day
+        mi = MES[fechaini.month]
+      else
+        mi = "#{fechafin.day}/#{fechafin.month}"
+      end
+      return mi + ai + " - " + mf + af
+    end
+
+
+    def victimizaciones
       authorize! :contar, Sivel2Gen::Caso
 
       # Filtros
@@ -32,6 +58,9 @@ module Sivel2Gen
       r.each {|h| 
         @mapa_depcuenta[h['departamento_divipola']]=h['count']
       }
+      @mapadep_titulo = 'Victimizaciones'
+      @mapadep_subtitulo = nomrango(pFini ? Date.new(pFini) : @conscaso.min(fecha), 
+                                  pFfin ? Date.new(pFfin) : @conscas.max(fecha))
 
       respond_to do |format|
         format.html { render 'mapadep_victimizaciones', layout: 'application' }
