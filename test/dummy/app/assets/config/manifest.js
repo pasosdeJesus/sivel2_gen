0b9e9c6800ab437d76f6da32e2076f0a0c5d0601
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-12-31 14:23:32 -0500

    Actualiza a rails7 lo del lado del cliente

--- test/dummy/app/assets/config/manifest.js
+++ test/dummy/app/assets/config/manifest.js
@@ -6,3 +6,7 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
+//= link_directory ../../../node_modules/chosen-js .png
+//= link application.css
+//= link_tree ../builds
