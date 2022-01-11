ace29ad000d74c2aa76c72c951296590bae549d3
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-01-11 12:26:41 -0500

    Quitamos @rails/ujs que es remplazado por mrujs

--- test/dummy/app/javascript/application.js
+++ test/dummy/app/javascript/application.js
@@ -22,11 +22,7 @@
 
 console.log('Hola Mundo desde Webpacker')
 
-import Rails from "@rails/ujs"
-Rails.start()
-
 import mrujs from "mrujs";
-import * as Turbo from "@hotwired/turbo"
 import "@hotwired/turbo-rails"
 
 mrujs.start();
