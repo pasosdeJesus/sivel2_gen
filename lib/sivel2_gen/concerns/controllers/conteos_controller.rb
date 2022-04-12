094af8fb8f3dd7950c08dffd8e86b019d0c16b4b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-04-12 15:06:20 -0500

    Presenta totales por filas y gran total en conteo de victimizaciones individuales. Closes #688

--- lib/sivel2_gen/concerns/controllers/conteos_controller.rb
+++ lib/sivel2_gen/concerns/controllers/conteos_controller.rb
@@ -527,6 +527,7 @@ module Sivel2Gen
 
           if params[:filtro] && params[:filtro][:agrucol] &&
             params[:filtro][:agrucol] == 'CATEGORÍA'
+            @totalesfila = true
 
             if que3.count == 3
               # Si no se desagrega, solo presenta tabla mínima con 
