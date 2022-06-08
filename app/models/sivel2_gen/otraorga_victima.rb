8c6a78f190f1cb08162f22c1686e4ec937cc1136
Luis Alejandro Cruz <alejocruzzz@gmail.com>
2022-06-08 09:02:44 -0500

    organizacion secundaria

---
+++ app/models/sivel2_gen/otraorga_victima.rb
@@ -0,0 +1,7 @@
+require 'sivel2_gen/concerns/models/otraorga_victima'
+
+module Sivel2Gen
+  class OtraorgaVictima < ActiveRecord::Base
+    include Sivel2Gen::Concerns::Models::OtraorgaVictima
+  end
+end
---
+++ db/migrate/20220608044102_crea_otra_orga_victima.rb
@@ -0,0 +1,14 @@
+class CreaOtraOrgaVictima < ActiveRecord::Migration[6.1]
+  def change
+    create_table :sivel2_gen_otraorga_victima, id: false do |t|
+      t.integer :organizacion_id
+      t.integer :victima_id
+      t.index :organizacion_id
+      t.index :victima_id
+    end
+    add_foreign_key :sivel2_gen_otraorga_victima, 
+      :sivel2_gen_organizacion, column: :organizacion_id
+    add_foreign_key :sivel2_gen_otraorga_victima, 
+      :sivel2_gen_victima, column: :victima_id
+  end
+end
