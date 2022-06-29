0f5d54704d5ec23dcf680d4a2b71fd1f1be81399
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-06-29 12:17:51 -0500

    Mejora migracion

--- db/migrate/20220608044102_crea_otra_orga_victima.rb
+++ db/migrate/20220608044102_crea_otra_orga_victima.rb
@@ -1,16 +1,20 @@
 class CreaOtraOrgaVictima < ActiveRecord::Migration[6.1]
-  def change
-    create_table :sivel2_gen_otraorga_victima, id: false do |t|
-      t.integer :organizacion_id
-      t.integer :victima_id
-      t.index :organizacion_id
-      t.index :victima_id
+  def up
+    if !table_exists?(:sivel2_gen_otraorga_victima) then
+      create_table :sivel2_gen_otraorga_victima, id: false do |t|
+        t.integer :organizacion_id
+        t.integer :victima_id
+        t.index :organizacion_id
+        t.index :victima_id
+      end
+      add_foreign_key :sivel2_gen_otraorga_victima,
+        :sivel2_gen_organizacion,
+        column: :organizacion_id
+      add_foreign_key :sivel2_gen_otraorga_victima,
+        :sivel2_gen_victima,
+        column: :victima_id
     end
-    add_foreign_key :sivel2_gen_otraorga_victima,
-                    :sivel2_gen_organizacion,
-                    column: :organizacion_id
-    add_foreign_key :sivel2_gen_otraorga_victima,
-                    :sivel2_gen_victima,
-                    column: :victima_id
   end
+    def down
+    end
 end
