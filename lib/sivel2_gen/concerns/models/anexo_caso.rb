ec0b8b448195068a99c33ddb78a58f8bcb133298
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-09-29 09:39:02 -0500

    Evita que se borren fuentes con anexo asociado cuando se borra anexo. Closes #803

--- lib/sivel2_gen/concerns/models/anexo_caso.rb
+++ lib/sivel2_gen/concerns/models/anexo_caso.rb
@@ -16,10 +16,10 @@ module Sivel2Gen
           accepts_nested_attributes_for :sip_anexo, reject_if: :all_blank
 
           has_many :caso_fuenteprensa, foreign_key: "anexo_caso_id",
-            dependent: :destroy, class_name: "Sivel2Gen::CasoFuenteprensa"
+            dependent: :nullify, class_name: "Sivel2Gen::CasoFuenteprensa"
 
           has_many :caso_fotra, foreign_key: "anexo_caso_id", 
-            dependent: :destroy, class_name: "Sivel2Gen::CasoFotra"
+            dependent: :nullify, class_name: "Sivel2Gen::CasoFotra"
 
           campofecha_localizado :fecha
 
