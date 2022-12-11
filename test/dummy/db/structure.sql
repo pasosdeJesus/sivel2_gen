e647bf8e64cf5c8b1b9c3c29b40de30f195b810b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-12-11 18:45:20 -0500

    Actualiza

--- test/dummy/db/structure.sql
+++ test/dummy/db/structure.sql
@@ -4072,7 +4072,7 @@ CREATE TABLE public.sivel2_gen_filiacion_victimacolectiva (
 --
 
 CREATE TABLE public.sivel2_gen_fotra (
-    id integer DEFAULT nextval(('sivel2_gen_fotra_id_seq'::text)::regclass) NOT NULL,
+    id integer DEFAULT nextval(('fuente_directa_seq'::text)::regclass) NOT NULL,
     nombre character varying(500) COLLATE public.es_co_utf_8,
     created_at timestamp without time zone,
     updated_at timestamp without time zone
@@ -7014,6 +7014,14 @@ ALTER TABLE ONLY public.sivel2_gen_categoria
 
 
 --
+-- Name: sivel2_gen_categoria categoria_contadaen_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sivel2_gen_categoria
+    ADD CONSTRAINT categoria_contadaen_fkey FOREIGN KEY (contadaen) REFERENCES public.sivel2_gen_categoria(id);
+
+
+--
 -- Name: sivel2_gen_contextovictima_victima contextovictima_victima_contextovictima_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -7902,6 +7910,14 @@ ALTER TABLE ONLY public.msip_persona
 
 
 --
+-- Name: sivel2_gen_presponsable presponsable_papa_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sivel2_gen_presponsable
+    ADD CONSTRAINT presponsable_papa_fkey FOREIGN KEY (papa_id) REFERENCES public.sivel2_gen_presponsable(id);
+
+
+--
 -- Name: sivel2_gen_caso_presponsable presuntos_responsables_caso_id_caso_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -8573,6 +8589,9 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20221209142327'),
 ('20221210155527'),
 ('20221211005549'),
-('20221211012152');
+('20221211012152'),
+('20221211141207'),
+('20221211141208'),
+('20221211141209');
 
 
