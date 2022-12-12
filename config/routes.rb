de669bf83b8e00539573d30b1705d76f525179aa
alejocruzrcc <alejocruzzz@gmail.com>
2022-12-12 06:24:46 -0500

    R825 para Msip (#866)
    * implemetne fuentes frecuentes con turbo
    
    * con caso para anexo
    
    * con msip

--- config/routes.rb
+++ config/routes.rb
@@ -37,8 +37,6 @@ Sivel2Gen::Engine.routes.draw do
 
   get "/fichacasovertical" => 'hogar#fichacasovertical'
 
-  get "/fuentesprensa/nuevo", 'fuentesprensa#nuevo'
-
   get '/graficar/actos_individuales' => 'fil23_gen/graficar_plotly#actos_individuales', 
     :as => 'graficar_actos_individuales'
 
@@ -75,6 +73,13 @@ Sivel2Gen::Engine.routes.draw do
 
   resources :casos, path_names: { new: 'nuevo', edit: 'edita' } do
       collection { post :importa }
+  end
+
+  resources :casos_fuenteprensa, only: [], param: :index do 
+    member do
+      delete '(:id)', to: "fuentesprensa#destroy", as: "eliminar"
+      post '/' => "fuentesprensa#create"
+    end
   end
 
   namespace :admin do
