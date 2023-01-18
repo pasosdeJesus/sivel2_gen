9f94965468ce22bc2e33870ad624cc908376a6c3
Luis Alejandro Cruz <alejocruzzz@gmail.com>
2023-01-17 19:53:17 -0500

    sin comentarios

--- lib/sivel2_gen/concerns/controllers/casopresponsables_controller.rb
+++ lib/sivel2_gen/concerns/controllers/casopresponsables_controller.rb
@@ -11,9 +11,7 @@ module Sivel2Gen
 
           load_and_authorize_resource class: Sivel2Gen::Caso
           before_action :prepara_caso
-          # Responde a requerimiento AJAX generado por cocoon creando una
-          # nueva fuente de prensa para el caso que recibe por parametro 
-          # params[:caso_id].  Pone valores simples en los campos requeridos
+
           def new
           end # def nuevo
 
