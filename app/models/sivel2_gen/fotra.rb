7a468e5e270b496baddf2bb65ca8449a5019fcb1
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-01-20 08:53:02 -0500

    Más consistencia otras fuente

--- app/models/sivel2_gen/fotra.rb
+++ app/models/sivel2_gen/fotra.rb
@@ -4,5 +4,7 @@ module Sivel2Gen
       class_name: 'Sivel2Gen::Fotra'
     has_many :anexo, foreign_key: "id_fotra", validate: true,
       class_name: 'Sivel2Gen::Anexo'
+
+    validates :nombre, presence: true
   end
 end
