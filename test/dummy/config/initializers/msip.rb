dca9b56dd0d2b979f6118a682b842c8cc3b5ac92
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-01-23 19:17:06 -0500

    Usa MSIP_RUTA_ANEXOS y MSIP_RUTA_VOLCADOS

--- test/dummy/config/initializers/msip.rb
+++ test/dummy/config/initializers/msip.rb
@@ -1,8 +1,10 @@
 require 'sivel2_gen/version'
 
 Msip.setup do |config|
-  config.ruta_anexos = "#{Rails.root}/archivos/anexos/"
-  config.ruta_volcados = "#{Rails.root}/archivos/bd/"
+  config.ruta_anexos = ENV.fetch('MSIP_RUTA_ANEXOS', 
+                                 "#{Rails.root}/archivos/anexos")
+  config.ruta_volcados = ENV.fetch('MSIP_RUTA_VOLCADOS',
+                                   "#{Rails.root}/archivos/bd")
   # En heroku los anexos son super-temporales
   if ENV["HEROKU_POSTGRESQL_MAUVE_URL"]
     config.ruta_anexos = "#{Rails.root}/tmp/"
