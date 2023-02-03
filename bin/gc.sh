3367767055ebdf40eff854a2821362c03444054d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-02-03 18:03:39 -0500

    Infraestructura

--- bin/gc.sh
+++ bin/gc.sh
@@ -74,7 +74,7 @@ if (test "$SINMIG" != "1") then {
   } fi;
 } fi;
 
-(cd $rutaap; RAILS_ENV=test bin/rails db:drop db:setup; RAILS_ENV=test bin/rails db:migrate sip:indices)
+(cd $rutaap; RAILS_ENV=test bin/rails db:environment:set RAILS_ENV=test; RAILS_ENV=test bin/rails db:drop db:create db:setup db:seed sip:indices)
 if (test "$?" != "0") then {
   echo "No puede preparse base de prueba";
   exit 1;
