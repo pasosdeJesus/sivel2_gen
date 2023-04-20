6f9de745548901f64c3ec9ec802ed662f6bb72e2
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-04-20 09:25:17 -0500

    Usa auxiliares genericos para convertir fechas. Resuelve https://gitlab.com/pasosdeJesus/si_fasol/-/issues/125

--- lib/sivel2_gen/concerns/controllers/buscarrepetidos_controller.rb
+++ lib/sivel2_gen/concerns/controllers/buscarrepetidos_controller.rb
@@ -15,34 +15,26 @@ module Sivel2Gen
 
             rf=""
 
-            if (params[:buscarrepetido] && 
-                params[:buscarrepetido][:fechaini] && 
+            if (params[:buscarrepetido] &&
+                params[:buscarrepetido][:fechaini] &&
                 params[:buscarrepetido][:fechaini] != '')
               pfi = params[:buscarrepetido][:fechaini]
-              if Rails.application.config.x.formato_fecha == 'dd-mm-yyyy'
-                pfid = Date.strptime(pfi, '%d-%m-%Y')
-              else
-                pfid = Date.strptime(pfi, '%Y-%m-%d')
-              end
-              rf += " AND c1.fecha >= #{pfid.strftime('%Y-%m-%d')}" +
-                " AND c2.fecha >= #{pfid.strftime('%Y-%m-%d')}" 
+              pfid = Msip::FormatoFechaHelper.fecha_local_estandar(pfi)
+              rf += " AND c1.fecha >= '#{pfid}'" +
+                " AND c2.fecha >= '#{pfid}'"
             end
-            if(params[:buscarrepetido] && params[:buscarrepetido][:fechafin] && 
+            if(params[:buscarrepetido] && params[:buscarrepetido][:fechafin] &&
                params[:buscarrepetido][:fechafin] != '')
               pff = params[:buscarrepetido][:fechafin]
-              if Rails.application.config.x.formato_fecha == 'dd-mm-yyyy'
-                pffd = Date.strptime(pff, '%d-%m-%Y')
-              else
-                pffd = Date.strptime(pff, '%Y-%m-%d')
-              end
-              rf += " AND c1.fecha <= #{pffd.strftime('%Y-%m-%d')}" +
-                " AND c2.fecha <= #{pffd.strftime('%Y-%m-%d')}" 
+              pffd = Msip::FormatoFechaHelper.fecha_local_estandar(pff)
+              rf += " AND c1.fecha <= '#{pffd}'" +
+                " AND c2.fecha <= '#{pffd}'"
             end
             res = Sivel2Gen::Caso.connection.select_all("
-              SELECT DISTINCT gr 
-              FROM (SELECT id, ARRAY(SELECT id FROM sivel2_gen_caso AS c2 
-                    WHERE c2.memo=c1.memo#{rf}) AS gr 
-                    FROM sivel2_gen_caso AS c1 ORDER BY id) AS s 
+              SELECT DISTINCT gr
+              FROM (SELECT id, ARRAY(SELECT id FROM sivel2_gen_caso AS c2
+                    WHERE c2.memo=c1.memo#{rf}) AS gr
+                    FROM sivel2_gen_caso AS c1 ORDER BY id) AS s
               WHERE array_length(gr, 1) > 1"
             )
             @repetidos << {
@@ -51,15 +43,15 @@ module Sivel2Gen
               cuerpo: res
             }
             respond_to do |format|
-              format.html { 
-                render 'sivel2_gen/buscarrepetidos/reportar', 
-                  layout: 'application' 
+              format.html {
+                render 'sivel2_gen/buscarrepetidos/reportar',
+                  layout: 'application'
               }
               format.json { head :no_content }
               format.js   { head :no_content }
             end
           end # def reportar
-          
+
           def buscarrepetidos_params
             params.require(:buscarrepetido).permit(
               :fechafin,
