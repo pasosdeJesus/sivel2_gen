d1ca2fc7301a2c8a090c3fc541c94b481a9b4188
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-06-02 08:06:29 -0500

    Mas validacion para presentar jerarquia

--- app/helpers/sivel2_gen/vista_helper.rb
+++ app/helpers/sivel2_gen/vista_helper.rb
@@ -7,7 +7,7 @@ module Sivel2Gen
     # Llamar dentro de <ul>..</ul>
     def presenta_arbol_presp_ul_li(nivel, arbol, opciones={})
       subarbol = ''
-      if arbol[:hijos].count > 0
+      if arbol && arbol[:hijos] && arbol[:hijos].count > 0
         subarbol = "<ul>\n".html_safe + arbol[:hijos].map{|h| 
           "<li>".html_safe + 
             presenta_arbol_presp_ul_li(nivel+1, h, opciones) +
@@ -15,12 +15,14 @@ module Sivel2Gen
         }.join("\n").html_safe +
         "</ul>\n".html_safe
       end
-      r = ''
-      if opciones[:conid] && !arbol[:id].nil?
-        r = CGI::escapeHTML("("+ arbol[:id].to_s + ") ")
+      r = "".dup
+      if arbol 
+        if opciones[:conid] && !arbol[:id].nil?
+          r << CGI::escapeHTML("("+ arbol[:id].to_s + ") ")
+        end
+        r << CGI::escapeHTML(arbol[:nombre].to_s) 
+        r << "\n" + subarbol
       end
-      r += CGI::escapeHTML(arbol[:nombre].to_s) 
-      r += "\n" + subarbol
       return r.html_safe
     end
     module_function :presenta_arbol_presp_ul_li
