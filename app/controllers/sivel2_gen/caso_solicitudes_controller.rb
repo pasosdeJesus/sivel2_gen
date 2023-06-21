85121803258f46e222c4b1b22b602d8665cc9c6c
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-06-21 13:28:56 -0500

    En formulario caso las solicitudes usan turbo en lugar de coccon y jquery

---
+++ app/controllers/sivel2_gen/caso_solicitudes_controller.rb
@@ -0,0 +1,8 @@
+require 'sivel2_gen/concerns/controllers/caso_solicitudes_controller'
+
+module Sivel2Gen
+  class CasoSolicitudesController < ApplicationController
+    load_and_authorize_resource class: Sivel2Gen::CasoSolicitud
+    include Sivel2Gen::Concerns::Controllers::CasoSolicitudesController
+  end
+end
