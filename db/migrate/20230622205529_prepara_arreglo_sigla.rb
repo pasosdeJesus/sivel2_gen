cacd85455f1e50e4b87dcb079b68f0d7d4052ce5
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-06-23 05:44:23 -0500

    Mejora migracion

--- db/migrate/20230622205529_prepara_arreglo_sigla.rb
+++ db/migrate/20230622205529_prepara_arreglo_sigla.rb
@@ -1,12 +1,12 @@
 class PreparaArregloSigla < ActiveRecord::Migration[7.0]
   def up
     execute <<-SQL
-      DROP VIEW sivel2_gen_conscaso1 CASCADE;
+      DROP VIEW IF EXISTS sivel2_gen_conscaso1 CASCADE;
     SQL
   end
   def down
     execute <<-SQL
-      DROP VIEW sivel2_gen_conscaso1 CASCADE;
+      DROP VIEW IF EXISTS sivel2_gen_conscaso1 CASCADE;
     SQL
   end
 end
