469a6eaf70e0bb7f0192118faf8a06cf903eb2dd
alejocruzrcc <alejocruzzz@gmail.com>
2023-06-23 19:04:15 -0500

    avance de anexo con turbo con error no handler de paperclip

--- lib/sivel2_gen/concerns/controllers/casos_controller.rb
+++ lib/sivel2_gen/concerns/controllers/casos_controller.rb
@@ -884,7 +884,8 @@ module Sivel2Gen
 
                 if params[:_msip_enviarautomatico] == "1"
                   params_finales = caso_params.except(
-                    :caso_etiqueta_attributes)
+                    :caso_etiqueta_attributes
+                  )
                  else
                    params_finales = caso_params
                  end
--- lib/sivel2_gen/concerns/models/anexo.rb
+++ lib/sivel2_gen/concerns/models/anexo.rb
@@ -6,12 +6,12 @@ module Sivel2Gen
     module Models
       module Anexo
         extend ActiveSupport::Concern
-        include Msip::Concerns::Models::Anexo
 
         included do
+          include Msip::Concerns::Models::Anexo
 
           has_many :anexo_caso, foreign_key: "anexo_id", 
-            validate: true, class_name: 'Sivel2Gen::AnexoCaso'
+            validate: true, class_name: 'Sivel2Gen::AnexoCaso', inverse_of: :msip_anexo
           has_many :caso, class_name: 'Sivel2Gen::Caso',
             through: :anexo_caso, inverse_of: :msip_anexo
         end
