e9dc919b9ece24ca98ada2b1dfcacbddba3439c5
alejocruzrcc <alejocruzzz@gmail.com>
2023-06-23 19:04:15 -0500

    avance de anexo con turbo con error no handler de paperclip

---
+++ app/controllers/msip/anexos_controller.rb
@@ -0,0 +1,10 @@
+require 'date'
+require 'sivel2_gen/concerns/controllers/anexos_controller' 
+
+module Msip
+  class AnexosController < Msip::ModelosController
+
+    load_and_authorize_resource class: Msip::Anexo
+    include Sivel2Gen::Concerns::Controllers::AnexosController
+  end
+end
---
+++ lib/sivel2_gen/concerns/controllers/anexos_controller.rb
@@ -0,0 +1,30 @@
+
+require 'msip/concerns/controllers/anexos_controller'
+require 'date'
+
+module Sivel2Gen
+  module Concerns
+    module Controllers
+      module AnexosController
+        extend ActiveSupport::Concern
+
+        included do
+          include Msip::Concerns::Controllers::AnexosController
+
+          def remplazar_antes_salvar_v
+            true
+          end 
+
+          def remplazar_despues_salvar_v
+            true
+          end 
+
+        end # included
+
+
+        class_methods do
+        end # class_methods
+      end
+    end
+  end
+end
