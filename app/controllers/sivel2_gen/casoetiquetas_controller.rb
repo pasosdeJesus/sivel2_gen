999273a45359f38b475883cd3c0033f08c6cd8d6
Luis Cruz <alejocruzzz@gmail.com>
2023-06-27 15:52:32 +0000

    F-Gl-254 cambia nomenclatura etiquetas y arregla problemas al borrar

--- app/controllers/sivel2_gen/casoetiquetas_controller.rb
+++
@@ -1,9 +0,0 @@
-require 'sivel2_gen/concerns/controllers/casoetiquetas_controller'
-
-module Sivel2Gen
-  class CasoetiquetasController < ApplicationController
-    load_and_authorize_resource class: Sivel2Gen::Caso
-
-    include Sivel2Gen::Concerns::Controllers::CasoetiquetasController
-  end
-end
