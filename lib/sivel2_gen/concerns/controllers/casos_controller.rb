66248ad34c8ff1c569ff452080c532cd57486c0f
alejocruzrcc <alejocruzzz@gmail.com>
2023-07-19 15:41:05 -0500

    Funciona y guarda bien con turbo y varias victimas y varios familiares tambien elimina bien

--- lib/sivel2_gen/concerns/controllers/casos_controller.rb
+++ lib/sivel2_gen/concerns/controllers/casos_controller.rb
@@ -864,9 +864,6 @@ module Sivel2Gen
                 params_finales = params[:caso].except(
                   :caso_anexo_attributes,
                   :caso_etiqueta_attributes,
-                  :caso_anexo_attributes,
-                  :victima_attributes,
-                  :victimacolectiva_attributes,
                   :caso_fuenteprensa_attributes,
                   :caso_fotra_attributes,
                   :caso_presponsable_attributes,
