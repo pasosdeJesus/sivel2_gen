eb497d12979867a094d2c6ebcfb786f26b9e8b48
alejocruzrcc <alejocruzzz@gmail.com>
2023-08-09 00:45:31 -0500

    arregla 2 fallas de familiares

--- lib/sivel2_gen/concerns/controllers/casos_controller.rb
+++ lib/sivel2_gen/concerns/controllers/casos_controller.rb
@@ -777,7 +777,7 @@ module Sivel2Gen
                 if victima_params[:persona_attributes]
                   if victima_params[:persona_attributes][:persona_trelacion1_attributes]
                     victima_params[:persona_attributes][:persona_trelacion1_attributes].each do |index_pt, pt_params|
-                      if pt_params[:id] == ""
+                      if pt_params[:id] == "" and pt_params[:_destroy] != "true"
                         p2_id = pt_params[:personados_attributes][:id]
                         if p2_id != ""
                           if victima_params[:id] != ""
