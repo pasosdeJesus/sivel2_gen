a7dc5e61dc36f22f138c755d1a09f4a38734516b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-11-20 18:46:59 -0500

    Simplifica

--- lib/sivel2_gen/concerns/controllers/caso_fuentesprensa_controller.rb
+++ lib/sivel2_gen/concerns/controllers/caso_fuentesprensa_controller.rb
@@ -8,10 +8,7 @@ module Sivel2Gen
           include ActionView::Helpers::AssetUrlHelper
 
           load_and_authorize_resource class: Sivel2Gen::Caso
-          before_action :prepara_caso
-
-          def new
-          end 
+          before_action :preparar_caso
 
           def destroy
           end
@@ -19,12 +16,9 @@ module Sivel2Gen
           def create
           end
 
-          def update
-          end
-
           private
 
-          def prepara_caso
+          def preparar_caso
             @caso = Sivel2Gen::Caso.new(
               caso_fuenteprensa: [Sivel2Gen::CasoFuenteprensa.new])
           end
