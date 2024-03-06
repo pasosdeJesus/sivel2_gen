0f2ad5bbea38475d3ef7895375f9a435aa740d64
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-03-06 07:32:59 -0500

    capa cargando modular

--- app/javascript/controllers/enviar_ficha_caso_controller.js
+++ app/javascript/controllers/enviar_ficha_caso_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   static targets = ["idcaso"]
 
   connect() {
-    document.getElementById("cargando").style.display = 'none';
+    document.getElementById("capa-cargando").style.display = 'none';
     console.log("Conexion de controlador enviar ficha caso establecida")
     const pestanias = document.querySelectorAll('.fichacambia');
     pestanias.forEach(function(pestania) {
@@ -38,7 +38,7 @@ export default class extends Controller {
 
   cambiarficha(){
     if(event.target.dataset.enviarFichaCasoTarget == 'actos-pestana'){
-      document.getElementById("cargando").style.display = 'flex';
+      document.getElementById("capa-cargando").style.display = 'flex';
       let casoId = this.idcasoTarget.value
       let puntomontaje = window.puntomontaje
       let url = puntomontaje + 'casos/' + casoId + '/guardar_y_editar'
@@ -76,7 +76,7 @@ export default class extends Controller {
                 localStorage.setItem('pestanaActiva', 'actos-pestana');
                 window.location = puntomontaje + 'casos/' + casoId + '/edita';
               } else {
-                document.getElementById("cargando").style.display = 'none';
+                document.getElementById("capa-cargando").style.display = 'none';
               }
            });
 
