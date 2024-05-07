60d58aba7a3ab312f17e319f15a68dfc1f57b223
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-05-07 08:43:09 -0500

    Compara nombres en el mismo locale dandole preferencia al de Colombia. Closes #645

--- lib/sivel2_gen/concerns/models/caso_presponsable.rb
+++ lib/sivel2_gen/concerns/models/caso_presponsable.rb
@@ -32,7 +32,7 @@ module Sivel2Gen
 
           def importa(datosent, datossal, menserror, opciones = {})
             pres = Sivel2Gen::Presponsable.
-              where(nombre: datosent['nombre_grupo'].upcase)
+              where('upper(nombre COLLATE es_co_utf_8)=upper(? COLLATE es_co_utf_8)', datosent['nombre_grupo'])
             unless pres.empty?
               self.presponsable_id = pres.ids[0]
               def recorrer_observaciones(ele)
