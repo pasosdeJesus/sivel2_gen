624fa81e513efcbe1ed035b32d4f12d304c04358
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-08-06 13:40:42 -0500

    Mejora mas migracion. #679

--- db/migrate/20240806082036_ref_anexo.rb
+++ db/migrate/20240806082036_ref_anexo.rb
@@ -6,7 +6,7 @@ class RefAnexo < ActiveRecord::Migration[7.1]
     SQL
     add_foreign_key :sivel2_gen_anexo_caso, :msip_anexo, column: :anexo_id
   end
-  def up
+  def down
     remove_foreign_key :sivel2_gen_anexo_caso, :msip_anexo, column: :anexo_id
   end
 end
