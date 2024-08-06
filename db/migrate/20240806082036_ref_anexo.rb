2b26e301f47313721a3bb94eafe0bcd7957fa6cd
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-08-06 13:40:42 -0500

    Mejora mas migracion. #679

--- db/migrate/20240806082036_ref_anexo.rb
+++ db/migrate/20240806082036_ref_anexo.rb
@@ -1,5 +1,12 @@
 class RefAnexo < ActiveRecord::Migration[7.1]
-  def change
+  def up
+    execute <<-SQL
+      DELETE FROM sivel2_gen_anexo_caso WHERE
+        anexo_id NOT IN (SELECT id FROM msip_anexo);
+    SQL
     add_foreign_key :sivel2_gen_anexo_caso, :msip_anexo, column: :anexo_id
+  end
+  def down
+    remove_foreign_key :sivel2_gen_anexo_caso, :msip_anexo, column: :anexo_id
   end
 end
