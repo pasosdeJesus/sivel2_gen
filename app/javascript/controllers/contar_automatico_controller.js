4a538af37aa091128b363959b79715b84f70048b
alejocruzrcc <alejocruzzz@gmail.com>
2024-09-06 09:30:07 -0500

    crea controlador independiente para contar automatico

---
+++ app/javascript/controllers/contar_automatico_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+import Msip__Motor from "../msip/motor"
+
+// Conecta con data-controller="sivel2-gen--contar-automatico"
+export default class extends Controller {
+  connect() {
+    console.log("conectado con contar automatico controller")
+  }
+
+  contar(){
+    const form = document.querySelector('form');
+    const action = form.action;
+    const formData = new FormData(form);
+    let data = new URLSearchParams(formData).toString();
+    data += '&commit=Contar';
+    const root = window
+    if (!root.dant || root.dant !== data) {
+      Msip__Motor.enviarAutomaticoFormulario(form, 'POST', 'script', true, 'Contar');
+    }
+    root.dant = data;
+  }
+
+}
