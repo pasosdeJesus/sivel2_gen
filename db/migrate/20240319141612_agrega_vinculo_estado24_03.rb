5fe07a68e34b076c1ab2efb8e545a5f40255a7f2
Nombre Usuario <correo@usuario.net>
2024-09-13 12:24:30 -0500

    Mejora migracion

--- db/migrate/20240319141612_agrega_vinculo_estado24_03.rb
+++ db/migrate/20240319141612_agrega_vinculo_estado24_03.rb
@@ -1,13 +1,13 @@
 class AgregaVinculoEstado2403 < ActiveRecord::Migration[7.1]
   def up
-    execute <<-SQL
-    INSERT INTO public.sivel2_gen_vinculoestado (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (44, 'EDIL', '2017-01-01', NULL, '2017-01-01', '2017-01-01', NULL);
-    INSERT INTO public.sivel2_gen_vinculoestado (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (45, 'JUSTICIA ESPECIAL PARA LA PAZ (JEP)', '2023-01-01', NULL, '2023-01-01', '2023-01-01', 'Víctima trabajaba en la JEP');
-    SQL
     if Sivel2Gen::Vinculoestado.where(id: 101).where(nombre: 'EDIL').count == 1
       execute <<-SQL
         UPDATE sivel2_gen_victima SET vinculoestado_id=44 WHERE vinculoestado_id=101;
         DELETE FROM sivel2_gen_vinculoestado WHERE id=101;
+      SQL
+    elsif Sivel2Gen::Vinculoestado.where(id: 44).count == 0
+      execute <<-SQL
+        INSERT INTO public.sivel2_gen_vinculoestado (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (44, 'EDIL', '2017-01-01', NULL, '2017-01-01', '2017-01-01', NULL);
       SQL
     end
     if Sivel2Gen::Vinculoestado.where(id: 102).where(nombre: 'JUSTICIA ESPECIAL PARA LA PAZ-JEP').count == 1
@@ -15,9 +15,13 @@ class AgregaVinculoEstado2403 < ActiveRecord::Migration[7.1]
         UPDATE sivel2_gen_victima SET vinculoestado_id=45 WHERE vinculoestado_id=102;
         DELETE FROM sivel2_gen_vinculoestado WHERE id=102;
       SQL
+    elsif Sivel2Gen::Vinculoestado.where(id: 45).count == 0
+      execute <<-SQL
+        INSERT INTO public.sivel2_gen_vinculoestado (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (45, 'JUSTICIA ESPECIAL PARA LA PAZ (JEP)', '2023-01-01', NULL, '2023-01-01', '2023-01-01', 'Víctima trabajaba en la JEP');
+      SQL
     end
-
   end
+
   def down
     execute <<-SQL
       DELETE FROM sivel2_gen_vinculoestado WHERE id IN (44, 45);
