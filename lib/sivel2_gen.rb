3cfbb714dd48487d5a4887499ded22fe35415ce1
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-10-09 06:55:18 -0500

    Mejora documentación. Closes #717

--- lib/sivel2_gen.rb
+++ lib/sivel2_gen.rb
@@ -1,5 +1,8 @@
 require "devise"
 require "sivel2_gen/engine"
 
+# Motor para manejar casos de violencia socio política.  Se basa en marco
+# conceptual del Banco de Datos del CINEP y en el modelo de datos 
+# "Who did what to whom" de HURIDOCS.
 module Sivel2Gen
 end
