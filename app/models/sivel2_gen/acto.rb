0c3e43b345364551fb685328605dcf73a8d36f9b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-10-09 06:55:18 -0500

    Mejora documentación. Closes #717

--- app/models/sivel2_gen/acto.rb
+++ app/models/sivel2_gen/acto.rb
@@ -3,7 +3,7 @@ require 'sivel2_gen/concerns/models/acto'
 
 module Sivel2Gen
   # Acto de violencia contra una persona, basado en modelo de datos
-  # "Who did what to whom" de Huridocs. Consta de: caso, presunto responsable,
+  # "Who did what to whom" de HURIDOCS. Consta de: caso, presunto responsable,
   # categoría de violencia, y persona (la víctima se identifica por el caso
   # y la persona).
   class Acto < ActiveRecord::Base
