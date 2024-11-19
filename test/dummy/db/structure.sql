907e6f7ce73366efe92be4f9587bed9325361476
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-11-19 15:55:34 -0500

    Agrega presuntos responsables a Estado y PSEI. Closes #743

--- test/dummy/db/structure.sql
+++ test/dummy/db/structure.sql
@@ -8560,6 +8560,7 @@ ALTER TABLE ONLY public.sivel2_gen_victimacolectiva_vinculoestado
 SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
+('20241119195733'),
 ('20241119180614'),
 ('20241113141404'),
 ('20241006115708'),
