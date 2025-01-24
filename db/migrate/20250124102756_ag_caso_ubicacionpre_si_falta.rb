ce84dbdeaf9d38713c8f423bd18fffb1940cd481
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-01-24 12:04:18 -0500

    Tabla caso_ubicacionpre, limpia ubicacion para conversion. Closes #759

---
+++ db/migrate/20250124102756_ag_caso_ubicacionpre_si_falta.rb
@@ -0,0 +1,26 @@
+class AgCasoUbicacionpreSiFalta < ActiveRecord::Migration[7.2]
+  def up
+    execute <<-SQL
+      CREATE SEQUENCE public.sivel2_gen_caso_ubicacionpre_id_seq
+          START WITH 1
+          INCREMENT BY 1
+          NO MINVALUE
+          NO MAXVALUE
+          CACHE 1;
+      CREATE TABLE public.sivel2_gen_caso_ubicacionpre (
+          id BIGINT PRIMARY KEY DEFAULT nextval('public.sivel2_gen_caso_ubicacionpre_id_seq'::regclass),
+          caso_id INTEGER REFERENCES public.sivel2_gen_caso(id),
+          ubicacionpre_id BIGINT REFERENCES public.msip_ubicacionpre(id),
+          latitud DOUBLE PRECISION,
+          longitud DOUBLE PRECISION,
+          tsitio_id INTEGER REFERENCES public.msip_tsitio
+      );
+    SQL
+  end
+  def down
+    execute <<-SQL
+      DROP TABLE public.sivel2_gen_caso_ubicacionpre;
+      DROP SEQUENCE public.sivel2_gen_caso_ubicacionpre_id_seq;
+    SQL
+  end
+end
