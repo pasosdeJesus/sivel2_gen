a3309cd113b43d1bd6a3a92478e084e6415394f7
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-02-18 11:39:46 -0500

    Mejorando rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,89 +1,88 @@
 # frozen_string_literal: true
 
 Sivel2Gen::Engine.routes.draw do
-  get "/acercade" => "hogar#acercade"
+  get "/acercade", action: "hogar#acercade"
 
-  get "/buscarrepetidos" => "buscarrepetidos#reportar", as: :buscarrepetidos
-  post "/buscarrepetidos" => "buscarrepetidos#reportar", as: :envia_buscarrepetidos
-  get "/victimas/nuevo"
-  get "victimas#nuevo"
+  get "/buscarrepetidos", action: "buscarrepetidos#reportar", as: :buscarrepetidos
+  post "/buscarrepetidos", action: "buscarrepetidos#reportar", 
+    as: :envia_buscarrepetidos
+  get "/victimas/nuevo", action: "victimas#nuevo"
 
-  get "/casos/busca" => "casos#busca"
-  get "/casos/cuenta" => "casos#cuenta"
-  get "/casos/datos-osm" => "casos#presenta_datos_mapaosm"
-  get "/casos/errores_importacion" => "casos#errores_importacion",
+  get "/casos/busca", action: "casos#busca"
+  get "/casos/cuenta", action: "casos#cuenta"
+  get "/casos/datos-osm", action: "casos#presenta_datos_mapaosm"
+  get "/casos/errores_importacion", action: "casos#errores_importacion",
     as: :casos_errores_importacion
-  get "/casos/importarrelatos" => "casos#importarrelatos"
-  get "/casos/:id/fichaimp" => "casos#fichaimp",
+  get "/casos/importarrelatos", action: "casos#importarrelatos"
+  get "/casos/:id/fichaimp", action: "casos#fichaimp",
     as: :caso_fichaimp
-  get "/casos/:id/fichapdf" => "casos#fichapdf",
+  get "/casos/:id/fichapdf", action: "casos#fichapdf",
     as: :caso_fichapdf
-  get "/casos/lista" => "casos#lista"
-  get "/casos/mapaosm" => "casos#mapaosm", as: :casos_mapaosm
-  get "/casos/refresca" => "casos#refresca", as: :casos_refresca
-  get "/admin/categorias/filtratviolencia" => "admin/categorias#filtra_por_tviolencia"
-  get "/combatientes/nuevo" => "combatientes#nuevo"
+  get "/casos/lista", action: "casos#lista"
+  get "/casos/mapaosm", action: "casos#mapaosm", as: :casos_mapaosm
+  get "/casos/refresca", action: "casos#refresca", as: :casos_refresca
+  get "/admin/categorias/filtratviolencia", action: "admin/categorias#filtra_por_tviolencia"
+  get "/combatientes/nuevo", action: "combatientes#nuevo"
 
-  get "/conteos/genvic" => "conteos#genvic", as: :conteos_genvic
-  get "/conteos/personas" => "conteos#personas", as: :conteos_personas
-  post "/conteos/personas" => "conteos#personas", as: :post_conteos_personas
-  get "/conteos/victimizaciones" => "conteos#victimizaciones",
+  get "/conteos/genvic", action: "conteos#genvic", as: :conteos_genvic
+  get "/conteos/personas", action: "conteos#personas", as: :conteos_personas
+  post "/conteos/personas", action: "conteos#personas", as: :post_conteos_personas
+  get "/conteos/victimizaciones", action: "conteos#victimizaciones",
     as: :conteos_victimizaciones
 
-  get "/fichacasovertical" => "hogar#fichacasovertical"
+  get "/fichacasovertical", action: "hogar#fichacasovertical"
 
-  get "/graficar/actos_individuales" => "fil23_gen/graficar_plotly#actos_individuales",
+  get "/graficar/actos_individuales", action: "fil23_gen/graficar_plotly#actos_individuales",
     :as => "graficar_actos_individuales"
 
-  get "/graficar/victimizaciones_individuales" => "graficar_victimizaciones_individuales#graficar",
-    #  get '/graficar/victimizaciones_individuales' => 'fil23_gen/graficar_apexcharts#victimizaciones_individuales',
+  get "/graficar/victimizaciones_individuales", action: "graficar_victimizaciones_individuales#graficar",
     :as => "graficar_victimizaciones_individuales"
 
-  post "/graficar/victimizaciones_individuales" => "graficar_victimizaciones_individuales#graficar",
+  post "/graficar/victimizaciones_individuales", action: "graficar_victimizaciones_individuales#graficar",
     :as => "enviar_graficar_victimizaciones_individuales"
 
-  get "/hogar" => "hogar#index"
+  get "/hogar", action: "hogar#index"
 
-  get "/admin/presponsables/nuevo" => "admin/presponsables#nuevo"
+  get "/admin/presponsables/nuevo", action: "admin/presponsables#nuevo"
 
-  get "/admin/presponsables/arbol" => "admin/presponsables#arbol",
+  get "/admin/presponsables/arbol", action: "admin/presponsables#arbol",
     as: :arbol_presponsables
 
-  get "/mapadep/victimizaciones" => "mapadep#victimizaciones",
+  get "/mapadep/victimizaciones", action: "mapadep#victimizaciones",
     as: :mapadepvic
 
-  get "/mapamun/victimizaciones" => "mapamun#victimizaciones",
+  get "/mapamun/victimizaciones", action: "mapamun#victimizaciones",
     as: :mapamunvic
 
-  get "/personas/remplazarfamiliar" => "/msip/personas#remplazarfamiliar"
+  get "/personas/remplazarfamiliar", action: "/msip/personas#remplazarfamiliar"
 
-  get "/tablasbasicas" => "hogar#tablasbasicas"
+  get "/tablasbasicas", action: "hogar#tablasbasicas"
 
-  get "/ubicaciones/nuevo" => "ubicaciones#nuevo"
+  get "/ubicaciones/nuevo", action: "ubicaciones#nuevo"
 
-  get "/validarcasos" => "validarcasos#validar", as: :validarcasos
-  post "/validarcasos" => "validarcasos#validar", as: :envia_validarcasos
+  get "/validarcasos", action: "validarcasos#validar", as: :validarcasos
+  post "/validarcasos", action: "validarcasos#validar", as: :envia_validarcasos
 
-  get "/victimascolectivas/nuevo" => "victimascolectivas#nuevo"
+  get "/victimascolectivas/nuevo", action: "victimascolectivas#nuevo"
 
   resources :actos, only: [], param: :index do
     member do
       delete "(:id)", to: "actos#destroy", as: "eliminar"
-      post "/" => "actos#create", as: "crear"
+      post "/", action: "actos#create", as: "crear"
     end
   end
 
   resources :actoscolectivos, only: [], param: :index do
     member do
       delete "(:id)", to: "actoscolectivos#destroy", as: "eliminar"
-      post "/" => "actoscolectivos#create", as: "crear"
+      post "/", action: "actoscolectivos#create", as: "crear"
     end
   end
 
   resources :anexo_casos, only: [], param: :index do
     member do
       delete "(:id)", to: "anexo_casos#destroy", as: "eliminar"
-      post "/" => "anexo_casos#create", as: "crear"
+      post "/", action: "anexo_casos#create", as: "crear"
     end
   end
 
@@ -98,49 +97,49 @@ Sivel2Gen::Engine.routes.draw do
   resources :caso_etiqueta, only: [], param: :index do
     member do
       delete "(:id)", to: "caso_etiquetas#destroy", as: "eliminar"
-      post "/" => "caso_etiquetas#create", as: "crear"
+      post "/", action: "caso_etiquetas#create", as: "crear"
     end
   end
 
   resources :caso_fotra, only: [], param: :index do
     member do
       delete "(:id)", to: "caso_fotras#destroy", as: "eliminar"
-      post "/" => "caso_fotras#create", as: "crear"
+      post "/", action: "caso_fotras#create", as: "crear"
     end
   end
 
   resources :caso_fuentesprensa, only: [], param: :index do
     member do
       delete "(:id)", to: "caso_fuentesprensa#destroy", as: "eliminar"
-      post "/" => "caso_fuentesprensa#create", as: "crear"
+      post "/", action: "caso_fuentesprensa#create", as: "crear"
     end
   end
 
   resources :caso_presponsable, only: [], param: :index do
     member do
       delete "(:id)", to: "caso_presponsables#destroy", as: "eliminar"
-      post "/" => "caso_presponsables#create", as: "crear"
+      post "/", action: "caso_presponsables#create", as: "crear"
     end
   end
 
   resources :caso_solicitud, only: [], param: :index do
     member do
       delete "(:id)", to: "caso_solicitudes#destroy", as: "eliminar"
-      post "/" => "caso_solicitudes#create", as: "crear"
+      post "/", action: "caso_solicitudes#create", as: "crear"
     end
   end
 
   resources :caso_ubicacionpre, only: [], param: :index do
     member do
       delete "(:id)", to: "caso_ubicacionespre#destroy", as: "eliminar"
-      post "/" => "caso_ubicacionespre#create", as: "crear"
+      post "/", action: "caso_ubicacionespre#create", as: "crear"
     end
   end
 
   resources :combatiente, only: [], param: :index do
     member do
       delete "(:id)", to: "combatientes#destroy", as: "eliminar"
-      post "/" => "combatientes#create", as: "crear"
+      post "/", action: "combatientes#create", as: "crear"
     end
   end
 
@@ -161,10 +160,10 @@ Sivel2Gen::Engine.routes.draw do
   resources :victimascolectivas, only: [], param: :index do
     member do
       delete "(:id)", to: "victimascolectivas#destroy", as: "eliminar"
-      post "/" => "victimascolectivas#create", as: "crear"
+      post "/", action: "victimascolectivas#create", as: "crear"
     end
   end
-  get "/victimas" => "victimas#index", as: "victimas"
+  get "/victimas", action: "victimas#index", as: "victimas"
 
   namespace :admin do
     ab = Ability.new
