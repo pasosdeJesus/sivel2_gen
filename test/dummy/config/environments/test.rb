c604365279e55734f3aedad41c5522b34f635fe4
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-06-28 15:44:17 -0500

    sin compresion css

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
   # While tests run files are not watched, reloading is not necessary.
   config.enable_reloading = false
 
+  config.assets.css_compressor = nil
+
   # Eager loading loads your entire application. When running a single test locally,
   # this is usually not necessary, and can slow down your test suite. However, it's
   # recommended that you enable it in continuous integration systems to ensure eager
